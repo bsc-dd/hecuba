a609bdb7a74fe64298c32330293772a831b181aa
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2016-11-24 18:49:10 +0100

    updated project structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from distutils.core import setup
+
+config = {'name': 'hecuba',
+          'version': '0.1',
+          'author': 'Guillem Alomar',
+          'author_email': 'guillem.alomar@bsc.es',
+          'url': 'https://www.bsc.es',
+          'install_requires': ['nose', 'cassandra-driver'],
+          'packages': ['hecuba']
+          }
+
+setup(**config)
---
+++ tests/Block_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class BlockTest(unittest.TestCase):
+    def static_creation_test(self):
+        self.fail('to be implemented')
+
+    def init_creation_test(self):
+        self.fail('to be implemented')
