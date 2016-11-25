158991c4ef442ed64287d97369de02eca39ca73a
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2016-11-25 19:08:01 +0100

    changed settings for easier testing

--- hecuba/settings.py
+++ hecuba/settings.py
@@ -2,27 +2,41 @@ def init():
     if 'apppath' not in globals():
         import os
         global apppath, contact_names, nodePort
-        apppath = os.environ['APPPATH']
         try:
-            nodePort = os.environ['NODEPORT']
-        except:
-            print 'using default nodePort 9042'
-            nodePort = 9042
+            apppath = os.environ['APPPATH']
+        except KeyError:
+            apppath = os.getcwd()
+    try:
+        nodePort = os.environ['NODEPORT']
+    except KeyError:
+        print 'using default nodePort 9042'
+        nodePort = 9042
 
-        try:
-            contact_names = os.environ['CONTACT_NAMES'].split(",")
-        except:
-            print 'using default contact point localhost'
-            contact_names = ['localhost']
+    try:
+        contact_names = os.environ['CONTACT_NAMES'].split(",")
+    except KeyError:
+        print 'using default contact point localhost'
+        contact_names = ['localhost']
     if 'cluster' not in globals():
         global cluster, session
-        from cassandra.cluster import Cluster
-        print 'CREATING GLOBAL SESSION'
-        cluster = Cluster(contact_points=contact_names, port=nodePort)
-        session = cluster.connect()
-        session.execute(
-            'CREATE TABLE IF NOT EXISTS hecuba.blocks (blockid text, block_classname text,storageobj_classname text, tkns list<bigint>, ' +
-            'entry_point text , port int, ksp text , tab text , dict_name text , obj_type text, PRIMARY KEY(blockid))')
+        if 'TESTING' in os.environ:
+            class clusterMock: pass
+            class sessionMock: pass
+            cluster = clusterMock()
+            session = sessionMock()
+        else:
+            from cassandra.cluster import Cluster
+            print 'CREATING GLOBAL SESSION'
+            cluster = Cluster(contact_points=contact_names, port=nodePort)
+            session = cluster.connect()
+            session.execute(
+                "CREATE KEYSPACE IF NOT EXISTS hecuba WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3 }")
+            session.execute(
+                'CREATE TABLE IF NOT EXISTS hecuba.blocks (blockid text, block_classname text,storageobj_classname text, tkns list<bigint>, ' +
+                'entry_point text , port int, ksp text , tab text , dict_name text , obj_type text, PRIMARY KEY(blockid))')
+
+    import logging
+    logging.basicConfig()
 
 
 init()
