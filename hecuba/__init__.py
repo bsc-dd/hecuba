2a3634d0884782460e11b88876c31b654ebcf752
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2016-12-03 17:30:20 +0100

    fixed config to be global

--- hecuba/__init__.py
+++ hecuba/__init__.py
@@ -1,6 +1,7 @@
 # author: G. Alomar
 import os
 import logging
+from cassandra.cluster import Cluster
 
 logging.basicConfig()
 
@@ -15,42 +16,53 @@ except ImportError:
 logging.getLogger('hecuba').addHandler(NullHandler())
 
 
-class Config(object):
-    def __init__(self):
-        self._configured = False
-        self._props = {}
+class Config:
+    class __Config:
+        def __init__(self):
+            self.configured = False
+
+    instance = __Config()
 
     def __getattr__(self, item):
-        if not self._configured:
-            self.reset()
-        return self._props[item]
+        return getattr(Config.instance, item)
 
-    def reset(self, mock_cassandra=False):
-        self._configured = True
+    def __init__(self, mock_cassandra=False):
+        Config.reset(mock_cassandra=mock_cassandra)
+
+    @staticmethod
+    def reset(mock_cassandra=False):
+        singleton = Config.instance
+        if singleton.configured and singleton.mock_cassandra == mock_cassandra:
+            logging.info('setting down')
+            return
+
+        singleton.mock_cassandra = mock_cassandra
+        logging.info('setting up configuration with mock_cassandra = %s', mock_cassandra)
+
+        singleton.configured = True
         if mock_cassandra:
             logging.info('configuring mock environment')
         else:
             logging.info('configuring production environment')
-
         try:
-            self._props['nodePort'] = int(os.environ['NODE_PORT'])
-            logging.info('NODE_PORT: %s', self._props['nodePort'])
+            singleton.nodePort = int(os.environ['NODE_PORT'])
+            logging.info('NODE_PORT: %s', singleton.nodePort)
         except KeyError:
             logging.warn('using default NODE_PORT 9042')
-            self._props['nodePort'] = 9042
+            singleton.nodePort = 9042
 
         try:
-            self._props['contact_names'] = os.environ['CONTACT_NAMES'].split(",")
-            logging.info('CONTACT_NAMES: %s', str.join(" ", self._props['contact_names']))
+            singleton.contact_names = os.environ['CONTACT_NAMES'].split(",")
+            logging.info('CONTACT_NAMES: %s', str.join(" ", singleton.contact_names))
         except KeyError:
             logging.warn('using default contact point localhost')
-            self._props['contact_names'] = ['localhost']
+            singleton.contact_names = ['localhost']
 
-        if hasattr(config, '') in globals():
+        if hasattr(singleton, 'session'):
             logging.warn('Shutting down pre-existent sessions and cluster')
             try:
-                self._props['session'].shutdown()
-                self._props['cluster'].shutdown()
+                singleton.session.shutdown()
+                singleton.cluster.shutdown()
             except:
                 logging.warn('error shutting down')
         if mock_cassandra:
@@ -62,87 +74,89 @@ class Config(object):
                     logging.info('called mock.session')
                     return []
 
-            self._props['cluster'] = clusterMock()
-            self._props['session'] = sessionMock()
+            singleton.cluster = clusterMock()
+            singleton.session = sessionMock()
 
         else:
-            from cassandra.cluster import Cluster
             logging.info('Initializing global session')
-            self._props['cluster'] = Cluster(contact_points=self._props['contact_names'], port=self._props['nodePort'])
-            self._props['session'] = self._props['cluster'].connect()
-            self._props['session'].execute(
-                "CREATE KEYSPACE IF NOT EXISTS hecuba WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3 }")
-            self._props['session'].execute(
-                'CREATE TABLE IF NOT EXISTS hecuba.blocks (blockid text, block_classname text,storageobj_classname text, tkns list<bigint>, ' +
-                'entry_point text , port int, ksp text , tab text , dict_name text , obj_type text, PRIMARY KEY(blockid))')
+            try:
+                singleton.cluster = Cluster(contact_points=singleton.contact_names, port=singleton.nodePort)
+                singleton.session = singleton.cluster.connect()
+                singleton.session.execute(
+                    "CREATE KEYSPACE IF NOT EXISTS hecuba WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3 }")
+                singleton.session.execute(
+                    'CREATE TABLE IF NOT EXISTS hecuba.blocks (blockid text, block_classname text,storageobj_classname text, tkns list<bigint>, ' +
+                    'entry_point text , port int, ksp text , tab text , dict_name text , obj_type text, PRIMARY KEY(blockid))')
+            except:
+                logging.error('Exception creating cluster session. Are you in a testing env?')
 
         try:
-            self._props['execution_name'] = os.environ['EXECUTION_NAME']
-            logging.info('EXECUTION_NAME: %s', self._props['execution_name'])
+            singleton.execution_name = os.environ['EXECUTION_NAME']
+            logging.info('EXECUTION_NAME: %s', singleton.execution_name)
         except KeyError:
-            self._props['execution_name'] = 'hecuba_app'
-            logging.warn('using default EXECUTION_NAME: %s', self._props['execution_name'])
+            singleton.execution_name = 'hecuba_app'
+            logging.warn('using default EXECUTION_NAME: %s', singleton.execution_name)
 
         try:
-            self._props['workers_per_node'] = int(os.environ['WORKERS_PER_NODE'])
-            logging.info('WORKERS_PER_NODE: %s', self._props['workers_per_node'])
+            singleton.workers_per_node = int(os.environ['WORKERS_PER_NODE'])
+            logging.info('WORKERS_PER_NODE: %s', singleton.workers_per_node)
         except KeyError:
-            self._props['workers_per_node'] = 8
-            logging.warn('using default WORKERS_PER_NODE: %s', self._props['workers_per_node'])
+            singleton.workers_per_node = 8
+            logging.warn('using default WORKERS_PER_NODE: %s', singleton.workers_per_node)
 
         try:
-            self._props['number_of_blocks'] = int(os.environ['NUMBER_OF_BLOCKS'])
-            logging.info('NUMBER_OF_BLOCKS: %s', self._props['number_of_blocks'])
+            singleton.number_of_blocks = int(os.environ['NUMBER_OF_BLOCKS'])
+            logging.info('NUMBER_OF_BLOCKS: %s', singleton.number_of_blocks)
         except KeyError:
-            self._props['number_of_blocks'] = 1024
-            logging.warn('using default NUMBER_OF_BLOCKS: %s', self._props['number_of_blocks'])
+            singleton.number_of_blocks = 1024
+            logging.warn('using default NUMBER_OF_BLOCKS: %s', singleton.number_of_blocks)
 
         try:
-            self._props['cache_activated'] = os.environ['CACHE_ACTIVATED'].lower() == 'true'
-            logging.info('CACHE_ACTIVATED: %s', self._props['cache_activated'])
+            singleton.cache_activated = os.environ['CACHE_ACTIVATED'].lower() == 'true'
+            logging.info('CACHE_ACTIVATED: %s', singleton.cache_activated)
         except KeyError:
-            self._props['cache_activated'] = True
-            logging.warn('using default RANGES_PER_BLOCK: %s', self._props['cache_activated'])
+            singleton.cache_activated = True
+            logging.warn('using default RANGES_PER_BLOCK: %s', singleton.cache_activated)
 
         try:
-            self._props['batch_size'] = int(os.environ['BATCH_SIZE'])
-            logging.info('BATCH_SIZE: %s', self._props['batch_size'])
+            singleton.batch_size = int(os.environ['BATCH_SIZE'])
+            logging.info('BATCH_SIZE: %s', singleton.batch_size)
         except KeyError:
-            self._props['batch_size'] = 100
-            logging.warn('using default BATCH_SIZE: %s', self._props['batch_size'])
+            singleton.batch_size = 100
+            logging.warn('using default BATCH_SIZE: %s', singleton.batch_size)
 
         try:
-            self._props['max_cache_size'] = int(os.environ['MAX_CACHE_SIZE'])
-            logging.info('MAX_CACHE_SIZE: %s', self._props['max_cache_size'])
+            singleton.max_cache_size = int(os.environ['MAX_CACHE_SIZE'])
+            logging.info('MAX_CACHE_SIZE: %s', singleton.max_cache_size)
         except KeyError:
-            self._props['max_cache_size'] = 100
-            logging.warn('using default MAX_CACHE_SIZE: %s', self._props['max_cache_size'])
+            singleton.max_cache_size = 100
+            logging.warn('using default MAX_CACHE_SIZE: %s', singleton.max_cache_size)
 
         try:
-            self._props['repl_factor'] = int(os.environ['REPLICA_FACTOR'])
-            logging.info('REPLICA_FACTOR: %s', self._props['repl_factor'])
+            singleton.repl_factor = int(os.environ['REPLICA_FACTOR'])
+            logging.info('REPLICA_FACTOR: %s', singleton.repl_factor)
         except KeyError:
-            self._props['repl_factor'] = 1
-            logging.warn('using default REPLICA_FACTOR: %s', self._props['repl_factor'])
+            singleton.repl_factor = 1
+            logging.warn('using default REPLICA_FACTOR: %s', singleton.repl_factor)
 
         try:
-            self._props['repl_class'] = os.environ['REPLICATION_STRATEGY']
-            logging.info('REPLICATION_STRATEGY: %s', self._props['repl_class'])
+            singleton.repl_class = os.environ['REPLICATION_STRATEGY']
+            logging.info('REPLICATION_STRATEGY: %s', singleton.repl_class)
         except KeyError:
-            self._props['repl_class'] = "SimpleStrategy"
-            logging.warn('using default REPLICATION_STRATEGY: %s', self._props['repl_class'])
+            singleton.repl_class = "SimpleStrategy"
+            logging.warn('using default REPLICATION_STRATEGY: %s', singleton.repl_class)
 
         try:
-            self._props['statistics_activated'] = os.environ['STATISTICS_ACTIVATED'].lower() == 'true'
-            logging.info('STATISTICS_ACTIVATED: %s', self._props['statistics_activated'])
+            singleton.statistics_activated = os.environ['STATISTICS_ACTIVATED'].lower() == 'true'
+            logging.info('STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
         except KeyError:
-            self._props['statistics_activated'] = True
-            logging.warn('using default STATISTICS_ACTIVATED: %s', self._props['statistics_activated'])
+            singleton.statistics_activated = True
+            logging.warn('using default STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
 
         try:
             query = "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : \'%s\', 'replication_factor' : %d};" \
-                    % (self._props['execution_name'], self._props['repl_class'], self._props['repl_factor'])
-            self._props['session'].execute(query)
+                    % (singleton.execution_name, singleton.repl_class, singleton.repl_factor)
+            singleton.session.execute(query)
         except Exception as e:
             print "Cannot create keyspace", e
 
