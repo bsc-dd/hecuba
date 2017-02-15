6b1daa37a320b826746859e4613078583ac638ee
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2017-02-15 16:24:29 +0100

    better logging

--- hecuba/__init__.py
+++ hecuba/__init__.py
@@ -5,15 +5,16 @@ from cassandra.cluster import Cluster
 from cassandra.policies import RetryPolicy
 
 
-# Set default logging handler to avoid "No handler found" warnings.
-try:  # Python 2.7+
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
+# Set default log.handler to avoid "No handler found" warnings.
 
-logging.getLogger('hecuba').addHandler(NullHandler())
+stderrLogger = logging.StreamHandler()
+f='%(filename)s: %(levelname)s: %(funcName)s(): %(lineno)d:\t%(message)s'
+stderrLogger.setFormatter(logging.Formatter(f))
+
+log = logging.getLogger('hecuba')
+log.addHandler(stderrLogger)
+#log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 
 
 class _NRetry(RetryPolicy):
@@ -56,44 +57,44 @@ class Config:
     def reset(mock_cassandra=False):
         singleton = Config.instance
         if singleton.configured and singleton.mock_cassandra == mock_cassandra:
-            logging.info('setting down')
+            log.info('setting down')
             return
 
         singleton.mock_cassandra = mock_cassandra
-        logging.info('setting up configuration with mock_cassandra = %s', mock_cassandra)
+        log.info('setting up configuration with mock_cassandra = %s', mock_cassandra)
 
         singleton.configured = True
         if mock_cassandra:
-            logging.info('configuring mock environment')
+            log.info('configuring mock environment')
         else:
-            logging.info('configuring production environment')
+            log.info('configuring production environment')
         try:
             singleton.nodePort = int(os.environ['NODE_PORT'])
-            logging.info('NODE_PORT: %d', singleton.nodePort)
+            log.info('NODE_PORT: %d', singleton.nodePort)
         except KeyError:
-            logging.warn('using default NODE_PORT 9042')
+            log.warn('using default NODE_PORT 9042')
             singleton.nodePort = 9042
 
         try:
             singleton.contact_names = os.environ['CONTACT_NAMES'].split(",")
-            logging.info('CONTACT_NAMES: %s', str.join(" ", singleton.contact_names))
+            log.info('CONTACT_NAMES: %s', str.join(" ", singleton.contact_names))
         except KeyError:
-            logging.warn('using default contact point localhost')
+            log.warn('using default contact point localhost')
             singleton.contact_names = ['127.0.0.1']
 
         if hasattr(singleton, 'session'):
-            logging.warn('Shutting down pre-existent sessions and cluster')
+            log.warn('Shutting down pre-existent sessions and cluster')
             try:
                 singleton.session.shutdown()
                 singleton.cluster.shutdown()
             except:
-                logging.warn('error shutting down')
+                log.warn('error shutting down')
         try:
             singleton.repl_factor = int(os.environ['REPLICA_FACTOR'])
-            logging.info('REPLICA_FACTOR: %d', singleton.repl_factor)
+            log.info('REPLICA_FACTOR: %d', singleton.repl_factor)
         except KeyError:
             singleton.repl_factor = 1
-            logging.warn('using default REPLICA_FACTOR: %d', singleton.repl_factor)
+            log.warn('using default REPLICA_FACTOR: %d', singleton.repl_factor)
 
         if mock_cassandra:
             class clusterMock:
@@ -102,7 +103,7 @@ class Config:
             class sessionMock:
 
                 def execute(self, *args, **kwargs):
-                    logging.info('called mock.session')
+                    log.info('called mock.session')
                     return []
 
                 def prepare(self, *args, **kwargs):
@@ -115,7 +116,7 @@ class Config:
             singleton.session = sessionMock()
 
         else:
-            logging.info('Initializing global session')
+            log.info('Initializing global session')
             try:
                 singleton.cluster = Cluster(contact_points=singleton.contact_names, port=singleton.nodePort,
                                             default_retry_policy=_NRetry(5))
@@ -137,82 +138,83 @@ class Config:
                     'PRIMARY KEY(storage_id))')
 
             except Exception as e:
-                logging.error('Exception creating cluster session. Are you in a testing env? %s', e)
+                log.error('Exception creating cluster session. Are you in a testing env? %s', e)
 
         try:
             singleton.execution_name = os.environ['EXECUTION_NAME']
-            logging.info('EXECUTION_NAME: %s', singleton.execution_name)
+            log.info('EXECUTION_NAME: %s', singleton.execution_name)
         except KeyError:
             singleton.execution_name = 'hecuba'
-            logging.warn('using default EXECUTION_NAME: %s', singleton.execution_name)
+            log.warn('using default EXECUTION_NAME: %s', singleton.execution_name)
 
         try:
             singleton.workers_per_node = int(os.environ['WORKERS_PER_NODE'])
-            logging.info('WORKERS_PER_NODE: %d', singleton.workers_per_node)
+            log.info('WORKERS_PER_NODE: %d', singleton.workers_per_node)
         except KeyError:
             singleton.workers_per_node = 8
-            logging.warn('using default WORKERS_PER_NODE: %d', singleton.workers_per_node)
+            log.warn('using default WORKERS_PER_NODE: %d', singleton.workers_per_node)
 
         try:
             singleton.number_of_blocks = int(os.environ['NUMBER_OF_BLOCKS'])
-            logging.info('NUMBER_OF_BLOCKS: %d', singleton.number_of_blocks)
+            log.info('NUMBER_OF_BLOCKS: %d', singleton.number_of_blocks)
         except KeyError:
             singleton.number_of_blocks = 1024
-            logging.warn('using default NUMBER_OF_BLOCKS: %d', singleton.number_of_blocks)
+            log.warn('using default NUMBER_OF_BLOCKS: %d', singleton.number_of_blocks)
 
         try:
             singleton.cache_activated = os.environ['CACHE_ACTIVATED'].lower() == 'true'
-            logging.info('CACHE_ACTIVATED: %s', singleton.cache_activated)
+            log.info('CACHE_ACTIVATED: %s', singleton.cache_activated)
         except KeyError:
             singleton.cache_activated = True
-            logging.warn('using default CACHE_ACTIVATED: %s', singleton.cache_activated)
+            log.warn('using default CACHE_ACTIVATED: %s', singleton.cache_activated)
 
         try:
             singleton.batch_size = int(os.environ['BATCH_SIZE'])
-            logging.info('BATCH_SIZE: %d', singleton.batch_size)
+            log.info('BATCH_SIZE: %d', singleton.batch_size)
         except KeyError:
             singleton.batch_size = 1
-            logging.warn('using default BATCH_SIZE: %d', singleton.batch_size)
+            log.warn('using default BATCH_SIZE: %d', singleton.batch_size)
 
         try:
             singleton.ranges_per_block = int(os.environ['RANGES_PER_BLOCK:'])
-            logging.info('RANGES_PER_BLOCK:: %d', singleton.ranges_per_block)
+            log.info('RANGES_PER_BLOCK:: %d', singleton.ranges_per_block)
         except KeyError:
             singleton.ranges_per_block = 1
-            logging.warn('using default RANGES_PER_BLOCK: %d', singleton.ranges_per_block)
+            log.warn('using default RANGES_PER_BLOCK: %d', singleton.ranges_per_block)
 
         try:
             singleton.max_cache_size = int(os.environ['MAX_CACHE_SIZE'])
-            logging.info('MAX_CACHE_SIZE: %d', singleton.max_cache_size)
+            log.info('MAX_CACHE_SIZE: %d', singleton.max_cache_size)
         except KeyError:
             singleton.max_cache_size = 100
-            logging.warn('using default MAX_CACHE_SIZE: %d', singleton.max_cache_size)
+            log.warn('using default MAX_CACHE_SIZE: %d', singleton.max_cache_size)
 
         try:
             singleton.repl_class = os.environ['REPLICATION_STRATEGY']
-            logging.info('REPLICATION_STRATEGY: %s', singleton.repl_class)
+            log.info('REPLICATION_STRATEGY: %s', singleton.repl_class)
         except KeyError:
             singleton.repl_class = "SimpleStrategy"
-            logging.warn('using default REPLICATION_STRATEGY: %s', singleton.repl_class)
+            log.warn('using default REPLICATION_STRATEGY: %s', singleton.repl_class)
 
         try:
             singleton.statistics_activated = os.environ['STATISTICS_ACTIVATED'].lower() == 'true'
-            logging.info('STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
+            log.info('STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
         except KeyError:
             singleton.statistics_activated = True
-            logging.warn('using default STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
+            log.warn('using default STATISTICS_ACTIVATED: %s', singleton.statistics_activated)
 
         try:
             singleton.prefetch_activated = os.environ['PREFETCH_ACTIVATED'].lower() == 'true'
         except KeyError:
             singleton.prefetch_activated = False
-            logging.warn('using default PREFETCH_ACTIVATED: %s', singleton.prefetch_activated)
+            log.warn('using default PREFETCH_ACTIVATED: %s', singleton.prefetch_activated)
 
         try:
             singleton.prefetch_size = int(os.environ['PREFETCH_SIZE'])
+            log.info('PREFETCH_SIZE: %s', singleton.prefetch_size)
         except KeyError:
             singleton.prefetch_size = 10000
-            logging.warn('using default PREFETCH_SIZE: %s', singleton.prefetch_activated)
+            log.warn('using default PREFETCH_SIZE: %s', singleton.prefetch_activated)
 
         try:
             query = "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : \'%s\'," \
