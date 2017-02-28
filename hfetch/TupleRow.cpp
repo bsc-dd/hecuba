bf5f8b0545550e431d84f4ce53bcff73f0320e66
polsm91 <pol.santamaria@bsc.es>
2017-02-28 17:33:54 +0100

    Solved bugs related to comparator and sharedptr

--- hfetch/TupleRow.cpp
+++ hfetch/TupleRow.cpp
@@ -32,43 +32,47 @@ TupleRow::TupleRow(const std::shared_ptr <std::vector<ColumnMeta>> metas,
 
 
 TupleRow::TupleRow(const TupleRow &t) {
+    this->payload_size=t.payload_size;
     this->payload = t.payload;
     this->metadata = t.metadata;
 }
 
 TupleRow::TupleRow(const TupleRow *t) {
+    this->payload_size=t->payload_size;
     this->payload = t->payload;
     this->metadata = t->metadata;
 }
 
 TupleRow::TupleRow(TupleRow &t) {
+    this->payload_size=t.payload_size;
     this->payload = t.payload;
     this->metadata = t.metadata;
 }
 
 TupleRow::TupleRow(TupleRow *t) {
+    this->payload_size=t->payload_size;
     this->payload = t->payload;
     this->metadata = t->metadata;
 }
 
 TupleRow &TupleRow::operator=(const TupleRow &t) {
+    this->payload_size=t.payload_size;
     this->payload = t.payload;
     this->metadata = t.metadata;
     return *this;
 }
 
 TupleRow &TupleRow::operator=(TupleRow &t) {
+    this->payload_size=t.payload_size;
     this->payload = t.payload;
     this->metadata = t.metadata;
     return *this;
 }
 
 bool operator<(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.payload_size != rhs.payload_size || lhs.metadata != rhs.metadata) {
-        return lhs.payload_size < rhs.payload_size;
-    } else {
-        return memcmp(lhs.payload.get(), rhs.payload.get(), lhs.payload_size) < 0;
-    }
+    if (lhs.payload_size != rhs.payload_size) return lhs.payload_size < rhs.payload_size;
+    if (lhs.metadata != rhs.metadata) return lhs.metadata <= rhs.metadata;
+    return memcmp(lhs.payload.get(), rhs.payload.get(), lhs.payload_size) < 0;
 }
 
 bool operator>(const TupleRow &lhs, const TupleRow &rhs) {
@@ -76,11 +80,10 @@ bool operator>(const TupleRow &lhs, const TupleRow &rhs) {
 }
 
 bool operator<=(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.payload_size != rhs.payload_size || lhs.metadata != rhs.metadata) {
-        return lhs.payload_size < rhs.payload_size;
-    } else {
-        return memcmp(lhs.payload.get(), rhs.payload.get(), lhs.payload_size) <= 0;
-    }
+
+    if (lhs.payload_size != rhs.payload_size) return lhs.payload_size < rhs.payload_size;
+    if (lhs.metadata != rhs.metadata) return lhs.metadata <= rhs.metadata;
+    return memcmp(lhs.payload.get(), rhs.payload.get(), lhs.payload_size) <= 0;
 }
 
 bool operator>=(const TupleRow &lhs, const TupleRow &rhs) {
@@ -88,6 +91,8 @@ bool operator>=(const TupleRow &lhs, const TupleRow &rhs) {
 }
 
 bool operator==(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.payload_size != rhs.payload_size || lhs.metadata != rhs.metadata) return false;
+
+    if (lhs.payload_size != rhs.payload_size) return lhs.payload_size < rhs.payload_size;
+    if (lhs.metadata != rhs.metadata) return lhs.metadata <= rhs.metadata;
     return memcmp(lhs.payload.get(), rhs.payload.get(), lhs.payload_size) == 0;
 }
