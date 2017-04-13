e3ef4172dd98452546f9f1d2558fe332591edeb3
polsm91 <pol.santamaria@bsc.es>
2017-04-13 12:45:47 +0200

    Changed to setuptools and shared libs use RPATH

--- setup.py
+++ setup.py
@@ -1,40 +1,72 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-#use_cython = False
-
-import os
-from distutils.core import setup, Extension
-from distutils.command.build import build as _build
 import subprocess
 
+from setuptools import setup, find_packages
+import setuptools.command.build_py
+import os
+import sys
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if '.so' in filename:
+                paths.append(os.path.join(path, filename))
+    return paths
 
 
 def cmake_build():
     if subprocess.call(["cmake", "-H./hfetch",  "-B./hfetch/build"]) != 0:
         raise EnvironmentError("error calling cmake")
 
-    if subprocess.call(["make","-C","./hfetch/build"]) != 0:
+    if subprocess.call(["make","-j4","-C","./hfetch/build"]) != 0:
         raise EnvironmentError("error calling make")
 
-class BuildWithCmake(_build):
-    def run(self):
+
+
+def setup_packages():
+
+    # We first build C++ libraries
+    if 'build' in sys.argv:
         cmake_build()
-        # can't use super() here because _build is an old style class in 2.7
-        _build.run(self)
+
+    extra_files = package_files('./hfetch/_install/lib') + package_files('./hfetch/_install/lib64')
+
+    #TODO use some flag to detect that build has already been done instead of this
+    if 'install' in sys.argv:
+        cmake_build()
+        extra_files = package_files('./hfetch/_install/lib') + package_files('./hfetch/_install/lib64')
+
+    # compute which libraries were built
+
+    metadata = dict(  name="Hecuba",
+        version="0.1",
+
+        packages=['hecuba', 'storage'], #find_packages(),
+
+        #install_requires=['nose', 'cassandra-driver', 'mock'],
+        zip_safe=False,
+        data_files=[('.',extra_files)],
+        include_package_data=True,
+
+        # metadata for upload to PyPI
+        license="PSF",
+        keywords="hello world example examples",
+        description='Hecuba',
+        author='Guillem Alomar,Cesare Cugnasco, Pol Santamaria, Yolanda Becerra ',
+        author_email='{guillem.alomar,cesare.cugnasco,pol.santamaria,yolanda.becerra}@bsc.es',
+        url='https://www.bsc.es',
+        long_description='''Hecuba.''',
+     #   cmdclass={              'build_py' : BuildWithCmake,              }
+    )
+
+    setup(**metadata)
+    return
 
 
-setup(name='Hecuba',
-      version='1.0',
-      description='Cache and prefetch for Hecuba',
-      author='Guillem Alomar,Cesare Cugnasco, Pol Santamaria, Yolanda Becerra ',
-      author_email='{guillem.alomar,cesare.cugnasco,pol.santamaria,yolanda.becerra}@bsc.es',
-      url='https://www.bsc.es',
-      install_requires=['nose', 'cassandra-driver', 'mock'],
-      packages=['hecuba', 'storage'],
-      data_files=[('lib/python2.7/site-packages/', ['hfetch/build/hfetch.so'])],
-      long_description='''Cache and prefetch for Hecuba.''',
-      cmdclass={
-          'build' : BuildWithCmake,
-          }
-      )
+
+if __name__ == '__main__':
+    setup_packages()
