3384903d9def1f595e9562f90417335f58df3d9c
polsm91 <pol.santamaria@bsc.es>
2017-06-21 10:38:30 +0200

    Test to verify KeyError a key is not found

--- hfetch/tests/python/CgetTest.py
+++ hfetch/tests/python/CgetTest.py
@@ -1,62 +1,66 @@
 import time
 from hfetch import *
 
-contact_names = 'minerva-5'
+contact_names = ['127.0.0.1']
+
 nodePort = 9042
-keyspace = 'case18'
+keyspace = 'test'
 table = 'particle'
-query_get = 'SELECT * FROM case18.particle WHERE partid=? AND time=? ;'
-query_token ='SELECT * FROM case18.particle WHERE token(partid)>? AND token(partid)<? ;'
 
 token_ranges = [(8070430489100699999,8070450532247928832)]
-num_keys = 1024
-size = 1024
+num_keys = 10001
+size = 10001
 
-success = connectCassandra(contact_names,nodePort)
-assert (success)
+try:
+    connectCassandra(contact_names,nodePort)
+except Exception:
+    print 'can\'t connect, verify the contact poins and port',contact_names,nodePort
 
-cache = Hcache(size,keyspace,table,query_get,[(8070430489100699999,8070450532247928832)],100,query_token)
+cache = Hcache(keyspace,table,"",token_ranges,["partid","time"],["ciao","x","y","z"],{'cache_size':size})
 
 
-
+result=None
 #clustering key
-ck = float(0.003)
 t1 = time.time()
-for pk in xrange(1, num_keys + 1):
+for pk in xrange(0, num_keys):
+    ck = pk*10
     try:
         result = cache.get_row([pk, ck])
-    except Exception as e:
-        print "Error when retrieving value from cache:", e
+    except KeyError as e:
+        print "Error when retrieving value from cache:", e, [pk,ck]
 print 'items in res: ', result
 print 'time - load C++ cache with cassandra data: ', time.time() - t1
 
 t1 = time.time()
-for pk in xrange(1, num_keys + 1):
+for pk in xrange(0, num_keys):
+    ck = pk*10
     try:
         result = cache.get_row([pk, ck])
-    except Exception as e:
-        print "Error when retrieving value from cache:", e
+    except KeyError as e:
+        print "Error when retrieving value from cache:", e, [pk,ck]
 # print 'items in res: ',len(result)
 print 'time - read data from C++ cache: ', time.time() - t1
 
 py_dict = {}
+cache = Hcache(keyspace,table,"",[(8070430489100699999,8070450532247928832)],["partid","time"],["ciao","x","y","z"],{'cache_size':num_keys})
 
 t1 = time.time()
-for pk in xrange(1, num_keys + 1):
+for pk in xrange(0, num_keys):
+    ck = pk*10
     try:
         py_dict[pk] = cache.get_row([pk, ck])
-    except Exception as e:
-        print "Error when retrieving value from cache:", e
+    except KeyError as e:
+        print "Error when retrieving value from cache:", e, [pk,ck]
 print 'time - load data into python dict: ', time.time() - t1
 # print 'size ', len(py_dict)
 # print 'items in res: ',len(py_dict[1])
 
 t1 = time.time()
-for pk in xrange(1, num_keys + 1):
+for pk in xrange(0, num_keys):
     try:
         result = py_dict[pk]
-    except Exception as e:
-        print "Error when retrieving value from cache:", e
+    except KeyError as e:
+        print "Error when retrieving value from cache:", e, [pk,ck]
 print 'time - read data from the python dict: ', time.time() - t1
 # print 'size ', len(py_dict)
 # print 'items in res: ',len(py_dict[1])
