452a160a6ec242753709b7f19f1742bca81e7262
polsm91 <pol.santamaria@bsc.es>
2017-06-23 17:44:15 +0200

    Numpy tests to unittest

--- hfetch/tests/python/numpy_tests.py
+++ hfetch/tests/python/numpy_tests.py
@@ -1,253 +1,288 @@
-import gc
-#gc.disable()
-#gc.set_debug(gc.DEBUG_STATS|gc.DEBUG_LEAK)
-from hfetch import *
-import numpy as np
+import unittest
 import time
-from cassandra.cluster import *
 
-
-def test_multidim():
-    dims = 3
-    elem_dim = 5
-
-    print 'Running multidimensional partition test'
-    print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
-
-    session.execute("DROP TABLE if exists test.arrays;")
-    session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image_block blob, image_block_pos int);")
-
-    a = Hcache(keyspace, "arrays", "WHERE token(partid)>=? AND token(partid)<?;",
-               [], ["partid"],
-               [{"name": "image_block", "type": "int", "dims": "5x5x5", "partition": "true"},
-                "image_block_pos"], {})
-
-    #prepare data
-    bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim, elem_dim)
-    temp =100
-    keys = [temp]
-    values = [bigarr.astype('i')]
-
-    #insert
-    a.put_row(keys, values)
-
-    # othw we ask for the row before it has been processed
-    time.sleep(2)
-
-    #retrieve
-    keys = [100]
-    result = a.get_row(keys)
-    if np.array_equal(bigarr,result[0]):
-        print 'Created and retrieved are equal'
-    else:
-        print 'Created and retrieved arrays differ, sth went wrong '
-        print 'Array sent ', bigarr
-        print 'Array retrieved ', result[0]
-    time.sleep(2)
-    session.execute("DROP TABLE test.arrays;")
-
-
-def nopart():
-
-    elem_dim = 128
-    txt_elem_dim = str(elem_dim)
-    dims = 2
-
-    print 'Running no partition test'
-    print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
-
-    session.execute("DROP TABLE if exists test.arrays;")
-    session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image_block blob);")
-
-
-    a = Hcache("test", "arrays", "WHERE token(partid)>=? AND token(partid)<?;",
-               [], ["partid"],
-               [{"name": "image_block",
-                 "type": "double",
-                 "dims": txt_elem_dim + 'x' + txt_elem_dim,
-                 "partition": "false"}], {})
-
-    keys = [300]
-    bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
-    bigarr.itemset(0, 14.0)
-    print 'Array to be written', bigarr.astype('d')
-
-    t1 = time.time()
-    a.put_row(keys, [bigarr.astype('d')])
-    print 'Elapsed time', time.time() - t1
-    print '2D, elem dimension: ', elem_dim
-
-    time.sleep(2)
-    session.execute("DROP TABLE test.arrays;")
+from hecuba import config
 
 
 
-def part():
+class Hfetch_Tests(unittest.TestCase):
+    keyspace = "test"
+    contact_names = ['127.0.0.1']
+    nodePort = 9042
 
-    dims = 2
-    elem_dim = 2048
-    txt_elem_dim = str(elem_dim)
+    @classmethod
+    def setUpClass(cls):
+        config.reset(mock_cassandra=False)
+        config.session.execute("CREATE KEYSPACE IF NOT EXISTS %s WITH replication "
+                               "= {'class': 'SimpleStrategy', 'replication_factor': 1};" % cls.keyspace)
+        time.sleep(5)
 
-    print 'Running partition test'
-    print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
+    @classmethod
+    def tearDownClass(cls):
+        #config.session.execute("DROP KEYSPACE IF EXISTS %s;" % cls.keyspace)
+        pass
 
-    session.execute("DROP TABLE if exists test.arrays;")
-    session.execute("CREATE TABLE test.arrays(partid int , image_block blob, image_block_pos int, PRIMARY KEY(partid,image_block_pos));")
+    def test_multidim(self):
+        from hfetch import connectCassandra
+        from hfetch import Hcache
+        import numpy as np
+        '''''''''
+        Multidimensional partition test: A numpy of more than 2 dimensions is partitioned and stored at the same table
+        
+        Analyzes:
+        
+        '''''''''
+        dims = 3
+        elem_dim = 5
 
-
-    a = Hcache("test", "arrays", "WHERE token(partid)>=? AND token(partid)<?;",
-               [(-8070430489100700000, 8070450532247928832)], ["partid"],
-               [{"name": "image_block",
-                 "type": "double",
-                 "dims": txt_elem_dim + 'x' + txt_elem_dim,
-                 "partition": "true"},
-                "image_block_pos"], {})
-
-    keys = [300]
-    bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
-    bigarr.itemset(0, 14.0)
-    print 'Array to be written', bigarr.astype('d')
-
-    t1 = time.time()
-    a.put_row(keys, [bigarr.astype('d')])
-    print 'Elapsed time', time.time() - t1
-    print '2D, elem dimension: ', elem_dim
-
-    time.sleep(2)
-    session.execute("DROP TABLE test.arrays;")
-
-
-
-def npy_uuid():
-
-    dims = 2
-    elem_dim = 2048
-    txt_elem_dim = str(elem_dim)
-
-    print 'Running npy_uuid test'
-    print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
-
-    session.execute("DROP TABLE if exists test.arrays;")
-    session.execute("DROP TABLE if exists test.arrays_aux;")
-    session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image uuid);")
-
-    session.execute("CREATE TABLE test.arrays_aux(uuid uuid,  position int, data blob, PRIMARY KEY (uuid,position));")
+        try:
+            connectCassandra(self.contact_names, self.nodePort)
+        except RuntimeError, e:
+            print e
+            print 'can\'t connect, verify the contact points and port', self.contact_names, self.nodePort
 
 
-    time.sleep(1)
+        table = "arrays"
 
-    a = Hcache("test", "arrays", "WHERE token(partid)>=? AND token(partid)<?;",
-               [], ["partid"],[{"name": "image",
-                                 "type": "double",
-                                 "dims": txt_elem_dim + 'x' + txt_elem_dim,
-                                 "partition": "true",
-                                 "npy_table": "arrays_aux"}], {})
+        config.session.execute("DROP TABLE if exists test.arrays;")
+        config.session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image_block blob, image_block_pos int);")
+
+        a = Hcache(self.keyspace, table, "WHERE token(partid)>=? AND token(partid)<?;",
+                   [], ["partid"],
+                   [{"name": "image_block", "type": "int", "dims": "5x5x5", "partition": "true"},
+                    "image_block_pos"], {})
+
+        #prepare data
+        bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim, elem_dim)
+        temp =100
+        keys = [temp]
+        values = [bigarr.astype('i')]
+
+        #insert
+        a.put_row(keys, values)
+
+        # othw we ask for the row before it has been processed
+        time.sleep(2)
+
+        #retrieve
+        keys = [100]
+        result = a.get_row(keys)
+        if np.array_equal(bigarr,result[0]):
+            print 'Created and retrieved are equal'
+        else:
+            print 'Created and retrieved arrays differ, sth went wrong '
+            print 'Array sent ', bigarr
+            print 'Array retrieved ', result[0]
+        time.sleep(2)
+        config.session.execute("DROP TABLE test.arrays;")
 
 
-    bigarr = np.arange(pow(elem_dim, 2)).reshape(elem_dim, elem_dim)
-    bigarr.itemset(0, 14.0)
-    #print 'Array to be written', bigarr.astype('d')
+    def test_nopart(self):
+        from hfetch import connectCassandra
+        from hfetch import Hcache
+        import numpy as np
+        '''''''''
+        No partition test: An array is stored without slicing as raw bytes.
+        
+        Analyzes:
+        
+        '''''''''
 
-    t1 = time.time()
-    #print a.get_row([300])
-    a.put_row( [300],[bigarr.astype('d')])
-
-    print 'Elapsed time', time.time() - t1
-    print '2D, elem dimension: ', elem_dim
-
-    time.sleep(3)
-    session.execute("DROP TABLE test.arrays;")
-    session.execute("DROP TABLE test.arrays_aux;")
+        elem_dim = 128
+        txt_elem_dim = str(elem_dim)
+        dims = 2
 
 
-def arr_put_get():
-    dims = 2
-    elem_dim = 2048
-    txt_elem_dim = str(elem_dim)
+        try:
+            connectCassandra(self.contact_names, self.nodePort)
+        except RuntimeError, e:
+            print e
+            print 'can\'t connect, verify the contact points and port', self.contact_names, self.nodePort
 
-    print 'Running arr_put_get test'
-    print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
 
-    session.execute("DROP TABLE if exists test.arrays;")
-    session.execute("CREATE TABLE test.arrays(partid int , image_block blob, image_block_pos int, PRIMARY KEY(partid,image_block_pos));")
+        table = "arrays"
 
-    time.sleep(3)
-    a = Hcache("test", "arrays", "WHERE token(partid)>=? AND token(partid)<?;",
-               [], ["partid"], [
-                   {"name": "image_block", "type": "double", "dims": txt_elem_dim + 'x' + txt_elem_dim,
-                    "partition": "true"}, "image_block_pos"], {})
+        config.session.execute("DROP TABLE if exists test.arrays;")
+        config.session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image_block blob);")
 
-    bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
-    bigarr.itemset(0, 14.0)
-    print 'Array to be written', bigarr.astype('d')
 
-    t1 = time.time()
-    keys =[300]
-    a.put_row(keys, [bigarr.astype('d')])
+        a = Hcache("test", table, "WHERE token(partid)>=? AND token(partid)<?;",
+                   [], ["partid"],
+                   [{"name": "image_block",
+                     "type": "double",
+                     "dims": txt_elem_dim + 'x' + txt_elem_dim,
+                     "partition": "false"}], {})
 
-    # othw we ask for the row before it has been processed
-    time.sleep(5)
+        keys = [300]
+        bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
+        bigarr.itemset(0, 14.0)
+        print 'Array to be written', bigarr.astype('d')
 
-    try:
-        result = a.get_row([300])
-        resarr = result[0]
-        print "And the result is... ", resarr.reshape((2048, 2048))
+        t1 = time.time()
+        a.put_row(keys, [bigarr.astype('d')])
+        print 'Elapsed time', time.time() - t1
+
+        time.sleep(2)
+        config.session.execute("DROP TABLE test.arrays;")
+
+
+
+    def test_part(self):
+        from hfetch import connectCassandra
+        from hfetch import Hcache
+        import numpy as np
+        '''''''''
+        Running partition test
+        
+        Analyzes:
+        
+        '''''''''
+
+        dims = 2
+        elem_dim = 2048
+        txt_elem_dim = str(elem_dim)
+
+        config.session.execute("DROP TABLE if exists test.arrays;")
+        config.session.execute("CREATE TABLE test.arrays(partid int , image_block blob, image_block_pos int, PRIMARY KEY(partid,image_block_pos));")
+
+        try:
+            connectCassandra(self.contact_names, self.nodePort)
+        except RuntimeError, e:
+            print e
+            print 'can\'t connect, verify the contact points and port', self.contact_names, self.nodePort
+
+        table = "arrays"
+
+        a = Hcache("test", table, "WHERE token(partid)>=? AND token(partid)<?;",
+                   [(-8070430489100700000, 8070450532247928832)], ["partid"],
+                   [{"name": "image_block",
+                     "type": "double",
+                     "dims": txt_elem_dim + 'x' + txt_elem_dim,
+                     "partition": "true"},
+                    "image_block_pos"], {})
+
+        keys = [300]
+        bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
+        bigarr.itemset(0, 14.0)
+        print 'Array to be written', bigarr.astype('d')
+
+        t1 = time.time()
+        a.put_row(keys, [bigarr.astype('d')])
         print 'Elapsed time', time.time() - t1
         print '2D, elem dimension: ', elem_dim
-    except KeyError:
-        print 'not found'
 
-    session.execute("DROP TABLE test.arrays;")
-
+        time.sleep(2)
+        config.session.execute("DROP TABLE test.arrays;")
 
 
 
-nodePort=9042
-host_list=["127.0.0.1"]
-keyspace = "test"
+    def test_npy_uuid(self):
+        from hfetch import connectCassandra
+        from hfetch import Hcache
+        import numpy as np
+        '''''''''
+        Running npy_uuid test
+        
+        Analyzes:
+        
+        '''''''''
 
-cluster = Cluster(host_list, nodePort)
-session = cluster.connect(keyspace)
+        dims = 2
+        elem_dim = 2048
+        txt_elem_dim = str(elem_dim)
+
+        print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
+        try:
+            connectCassandra(self.contact_names, self.nodePort)
+        except RuntimeError, e:
+            print e
+            print 'can\'t connect, verify the contact points and port', self.contact_names, self.nodePort
+
+        table = "arrays"
+
+        config.session.execute("DROP TABLE if exists test.arrays;")
+        config.session.execute("DROP TABLE if exists test.arrays_aux;")
+        config.session.execute("CREATE TABLE test.arrays(partid int PRIMARY KEY, image uuid);")
+
+        config.session.execute("CREATE TABLE test.arrays_aux(uuid uuid,  position int, data blob, PRIMARY KEY (uuid,position));")
 
 
+        time.sleep(1)
+
+        a = Hcache("test", table, "WHERE token(partid)>=? AND token(partid)<?;",
+                   [], ["partid"],[{"name": "image",
+                                     "type": "double",
+                                     "dims": txt_elem_dim + 'x' + txt_elem_dim,
+                                     "partition": "true",
+                                     "npy_table": "arrays_aux"}], {})
 
 
-if __name__ == '__main__':
+        bigarr = np.arange(pow(elem_dim, 2)).reshape(elem_dim, elem_dim)
+        bigarr.itemset(0, 14.0)
+        #print 'Array to be written', bigarr.astype('d')
 
-    connectCassandra(host_list,nodePort)
+        t1 = time.time()
+        #print a.get_row([300])
+        a.put_row( [300],[bigarr.astype('d')])
 
-    ''''''''' PENDING
-    '''''''''
-    for i in xrange(0,5):
-        test_multidim()
-        print 'test done, collecting'
-        gc.collect()
-        print 'collection done'
-        time.sleep(4)
-        nopart()
-        time.sleep(4)
-        print 'test done, collecting'
-        gc.collect()
-        print 'collection done'
-        npy_uuid()
-        time.sleep(4)
-        print 'test done, collecting'
-        gc.collect()
-        print 'collection done'
-        arr_put_get()
-        time.sleep(4)
-        print 'test done, collecting'
-        gc.collect()
-        print 'collection done'
-        part()
-        time.sleep(4)
-        print 'test done, collecting'
-        gc.collect()
-        print 'collection done'
-    ''''''''' DONE
-    '''''''''
+        print 'Elapsed time', time.time() - t1
+        print '2D, elem dimension: ', elem_dim
 
-    wait = raw_input("End test?")
-    gc.collect()
-    cluster.shutdown()
-    print 'bye'
+        time.sleep(3)
+        config.session.execute("DROP TABLE test.arrays;")
+        config.session.execute("DROP TABLE test.arrays_aux;")
+
+
+    def test_arr_put_get(self):
+        from hfetch import connectCassandra
+        from hfetch import Hcache
+        import numpy as np
+        '''''''''
+        Running arr_put_get test
+        
+        Analyzes:
+        
+        '''''''''
+        dims = 2
+        elem_dim = 2048
+        txt_elem_dim = str(elem_dim)
+
+        print 'Dimensions: ', dims, ' Element in each dim: ', elem_dim
+        try:
+            connectCassandra(self.contact_names, self.nodePort)
+        except RuntimeError, e:
+            print e
+            print 'can\'t connect, verify the contact points and port', self.contact_names, self.nodePort
+
+        table = "arrays"
+
+        config.session.execute("DROP TABLE if exists test.arrays;")
+        config.session.execute("CREATE TABLE test.arrays(partid int , image_block blob, image_block_pos int, PRIMARY KEY(partid,image_block_pos));")
+
+        time.sleep(3)
+        a = Hcache("test", table, "WHERE token(partid)>=? AND token(partid)<?;",
+                   [], ["partid"], [
+                       {"name": "image_block", "type": "double", "dims": txt_elem_dim + 'x' + txt_elem_dim,
+                        "partition": "true"}, "image_block_pos"], {})
+
+        bigarr = np.arange(pow(elem_dim, dims)).reshape(elem_dim, elem_dim)
+        bigarr.itemset(0, 14.0)
+        print 'Array to be written', bigarr.astype('d')
+
+        t1 = time.time()
+        keys =[300]
+        a.put_row(keys, [bigarr.astype('d')])
+
+        # othw we ask for the row before it has been processed
+        time.sleep(5)
+
+        try:
+            result = a.get_row([300])
+            resarr = result[0]
+            print "And the result is... ", resarr.reshape((2048, 2048))
+            print 'Elapsed time', time.time() - t1
+            print '2D, elem dimension: ', elem_dim
+        except KeyError:
+            print 'not found'
+
+        config.session.execute("DROP TABLE test.arrays;")
