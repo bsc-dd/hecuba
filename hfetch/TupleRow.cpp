2377d53996aca03ed1f38eab97038e7d15ffad41
polsm91 <pol.santamaria@bsc.es>
2017-06-27 17:27:38 +0200

    Code reformat TupleRow

--- hfetch/TupleRow.cpp
+++ hfetch/TupleRow.cpp
@@ -1,34 +1,33 @@
-
 #include "TupleRow.h"
 
 
-TupleRow::TupleRow(std::shared_ptr <const std::vector<ColumnMeta>> metas,
-                   uint16_t payload_size, void *buffer) {
+TupleRow::TupleRow(std::shared_ptr<const std::vector<ColumnMeta>> metas,
+                   uint32_t payload_size, void *buffer) {
 
     metadatas = metas;
-    payload = std::shared_ptr <TupleRowData >(new TupleRowData(buffer,payload_size),
-                                    [metas](TupleRowData *holder) {
-                                        for (uint16_t i=0; i<metas->size(); ++i) {
-                                            switch ( metas->at(i).type) {
-                                                case CASS_VALUE_TYPE_BLOB:
-                                                case CASS_VALUE_TYPE_TEXT:
-                                                case CASS_VALUE_TYPE_VARCHAR:
-                                                case CASS_VALUE_TYPE_UUID:
-                                                case CASS_VALUE_TYPE_ASCII: {
-                                                    if (!(holder->null_values&(0x1<<i))) {
-                                                        int64_t *addr = (int64_t * )((char *) holder->data +  metas->at(i).position);
-                                                        char *d = reinterpret_cast<char *>(*addr);
-                                                        free(d);
+    payload = std::shared_ptr<TupleRowData>(new TupleRowData(buffer, payload_size),
+                                            [metas](TupleRowData *holder) {
+                                                for (uint16_t i = 0; i < metas->size(); ++i) {
+                                                    switch (metas->at(i).type) {
+                                                        case CASS_VALUE_TYPE_BLOB:
+                                                        case CASS_VALUE_TYPE_TEXT:
+                                                        case CASS_VALUE_TYPE_VARCHAR:
+                                                        case CASS_VALUE_TYPE_UUID:
+                                                        case CASS_VALUE_TYPE_ASCII: {
+                                                            if (!holder->isNull(i)) {
+                                                                int64_t *addr = (int64_t *) ((char *) holder->data +
+                                                                                             metas->at(i).position);
+                                                                char *d = reinterpret_cast<char *>(*addr);
+                                                                free(d);
+                                                            }
+                                                            break;
+                                                        }
+                                                        default:
+                                                            break;
                                                     }
-                                                    break;
                                                 }
-                                                default:
-                                                    break;
-                                            }
-                                        }
-                                        delete(holder);
-
-                                    });
+                                                delete (holder);
+                                            });
 }
 
 
@@ -65,8 +64,8 @@ TupleRow &TupleRow::operator=(TupleRow &t) {
 }
 
 bool operator<(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.metadatas!=rhs.metadatas) return lhs.metadatas<rhs.metadatas;
-   return *lhs.payload.get()<*rhs.payload.get();
+    if (lhs.metadatas != rhs.metadatas) return lhs.metadatas < rhs.metadatas;
+    return *lhs.payload.get() < *rhs.payload.get();
 }
 
 bool operator>(const TupleRow &lhs, const TupleRow &rhs) {
@@ -74,8 +73,8 @@ bool operator>(const TupleRow &lhs, const TupleRow &rhs) {
 }
 
 bool operator<=(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.metadatas!=rhs.metadatas) return lhs.metadatas<rhs.metadatas;
-    return *lhs.payload.get()<=*rhs.payload.get();
+    if (lhs.metadatas != rhs.metadatas) return lhs.metadatas < rhs.metadatas;
+    return *lhs.payload.get() <= *rhs.payload.get();
 }
 
 bool operator>=(const TupleRow &lhs, const TupleRow &rhs) {
@@ -83,6 +82,6 @@ bool operator>=(const TupleRow &lhs, const TupleRow &rhs) {
 }
 
 bool operator==(const TupleRow &lhs, const TupleRow &rhs) {
-    if (lhs.metadatas!=rhs.metadatas) return false;
-    return *lhs.payload.get()==*rhs.payload.get();
+    if (lhs.metadatas != rhs.metadatas) return false;
+    return *lhs.payload.get() == *rhs.payload.get();
 }
--- hfetch/TupleRow.h
+++ hfetch/TupleRow.h
@@ -1,5 +1,5 @@
-#ifndef PREFETCHER_MY_TUPLE_H
-#define PREFETCHER_MY_TUPLE_H
+#ifndef TUPLEROW_H
+#define TUPLEROW_H
 
 #include <iostream>
 #include <memory>
@@ -14,66 +14,14 @@
 
 
 class TupleRow {
-private:
-    struct TupleRowData {
-        void* data;
-        uint32_t null_values;
-        uint32_t length;
-
-        TupleRowData(void* data_ptr, uint32_t length) {
-            this->data=data_ptr;
-            this->null_values=0;
-            this->length = length;
-        }
-
-        ~TupleRowData() {
-            free(data);
-        }
-
-        bool operator<(TupleRowData &rhs) {
-            if (this->length != rhs.length) return this->length < rhs.length;
-            if (this->null_values!=rhs.null_values)
-                return this->null_values < rhs.null_values;
-            return memcmp(this->data, rhs.data, this->length) < 0;
-        }
-
-        bool operator>(TupleRowData &rhs) {
-            return rhs < *this;
-        }
-
-        bool operator<=(TupleRowData &rhs) {
-            if (this->length != rhs.length) return this->length < rhs.length;
-            if (this->null_values!=rhs.null_values)
-                return this->null_values < rhs.null_values;
-            return memcmp(this->data, rhs.data, this->length) <= 0;
-        }
-
-        bool operator>=(TupleRowData &rhs) {
-            return rhs <= *this;
-        }
-
-        bool operator==(TupleRowData &rhs) {
-            if (this->length != rhs.length) return this->length < rhs.length;
-            if (this->null_values!=rhs.null_values)
-                return this->null_values < rhs.null_values;
-            return memcmp(this->data, rhs.data, length) == 0;
-        }
-
-    };
-
-
-
-    std::shared_ptr<TupleRowData> payload;
-    std::shared_ptr<const std::vector<ColumnMeta>> metadatas;
 
 public:
 
     /* Constructor */
-    TupleRow(std::shared_ptr<const std::vector<ColumnMeta>> metas, uint16_t payload_size,void *buffer);
-
+    TupleRow(std::shared_ptr<const std::vector<ColumnMeta> > metas, uint32_t payload_size, void *buffer);
 
     /* Copy constructors */
-    TupleRow(const TupleRow &t) ;
+    TupleRow(const TupleRow &t);
 
     TupleRow(const TupleRow *t);
 
@@ -81,27 +29,26 @@ public:
 
     TupleRow(TupleRow &t);
 
-    TupleRow& operator=( const TupleRow& other );
+    TupleRow &operator=(const TupleRow &other);
 
-    TupleRow& operator=(TupleRow& other );
+    TupleRow &operator=(TupleRow &other);
 
 
     /* Set methods */
-//TODO implement this ops with atomic_exchange
-    void setNull(uint32_t position) {
-        this->payload->null_values= this->payload->null_values|(0x1<<position);
+    inline void setNull(uint32_t position) {
+        this->payload->setNull(position);
     }
 
-    void unsetNull(uint32_t position) {
-        this->payload->null_values= this->payload->null_values &!(0x1<<position);
+    inline void unsetNull(uint32_t position) {
+        this->payload->unsetNull(position);
     }
 
     /* Get methods */
-    bool isNull(uint32_t position) const {
-        return  this->payload->null_values&(0x1<<position);
+    inline bool isNull(uint32_t position) const {
+        return this->payload->isNull(position);
     }
 
-    inline void* get_payload() const{
+    inline void *get_payload() const {
         return this->payload->data;
     }
 
@@ -109,14 +56,13 @@ public:
         return (uint16_t) metadatas->size();
     }
 
-    inline const void* get_element(int32_t position) const {
-        if (position < 0 || payload->data == nullptr) return nullptr;
-        if (isNull((uint32_t)position)) return nullptr;
-        return (const char *) payload->data + metadatas->at(position).position;
+    inline const void *get_element(uint32_t position) const {
+        if (!isNull(position))
+            return (char *) payload->data + metadatas->at(position).position;
+        return nullptr;
     }
 
     /* Comparision operators */
-
     friend bool operator<(const TupleRow &lhs, const TupleRow &rhs);
 
     friend bool operator>(const TupleRow &lhs, const TupleRow &rhs);
@@ -128,6 +74,77 @@ public:
     friend bool operator==(const TupleRow &lhs, const TupleRow &rhs);
 
 
+private:
+
+    struct TupleRowData {
+
+        /* Attributes */
+        void *data;
+        uint32_t null_values, length;
+
+
+        /* Constructors */
+        TupleRowData(void *data_ptr, uint32_t length) {
+            this->data = data_ptr;
+            this->null_values = 0;
+            this->length = length;
+        }
+
+        /* Destructors */
+        ~TupleRowData() {
+            free(data);
+        }
+
+        /* Modifiers */
+        void setNull(uint32_t position) {
+            this->null_values |= (0x1 << position);
+        }
+
+        void unsetNull(uint32_t position) {
+            this->null_values &= !(0x1 << position);
+        }
+
+        /* Get methods */
+        bool isNull(uint32_t position) const {
+            return data == nullptr || (this->null_values & (0x1 << position)) > 0;
+        }
+
+
+        /* Comparators */
+        bool operator<(TupleRowData &rhs) {
+            if (this->length != rhs.length) return this->length < rhs.length;
+            if (this->null_values != rhs.null_values)
+                return this->null_values < rhs.null_values;
+            return memcmp(this->data, rhs.data, this->length) < 0;
+        }
+
+        bool operator>(TupleRowData &rhs) {
+            return rhs < *this;
+        }
+
+        bool operator<=(TupleRowData &rhs) {
+            if (this->length != rhs.length) return this->length < rhs.length;
+            if (this->null_values != rhs.null_values)
+                return this->null_values < rhs.null_values;
+            return memcmp(this->data, rhs.data, this->length) <= 0;
+        }
+
+        bool operator>=(TupleRowData &rhs) {
+            return rhs <= *this;
+        }
+
+        bool operator==(TupleRowData &rhs) {
+            if (this->length != rhs.length) return this->length < rhs.length;
+            if (this->null_values != rhs.null_values)
+                return this->null_values < rhs.null_values;
+            return memcmp(this->data, rhs.data, length) == 0;
+        }
+
+    };
+
+
+    std::shared_ptr<TupleRowData> payload;
+    std::shared_ptr<const std::vector<ColumnMeta>> metadatas;
 };
 
-#endif //PREFETCHER_MY_TUPLE_H
+#endif //TUPLEROW_H
