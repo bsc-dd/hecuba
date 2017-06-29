ca0ee463366d36d2d05f9a75bc0d5f0a65c79acf
cugni <cesare.cugnasco@gmail.com>
2017-06-29 16:32:39 +0200

    cleaning all files

--- prefetcher/tests/python/do.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append(".")
-from hfetch import *
-connectCassandra(["minerva-5"],9042)
-a = Hcache(10,"case18","particle","WHERE token(partid)>=? AND token(partid)<?;",[(8070430489100700000,8070450532247928832)],["partid","time"],["time","x"])
-itera = a.iteritems(123)
-print itera.get_next()
-wait = raw_input("End test?")
