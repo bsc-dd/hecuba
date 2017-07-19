50fe56b9c0dfc20f47ccb3ade09d2e417f109aac
guillemnicolau <guillemnicolau@gmail.com>
2017-07-19 10:53:37 +0200

    Bug fixes

--- storage/api.py
+++ storage/api.py
@@ -112,5 +112,5 @@ def getByID(objid):
     cname = class_name[last + 1:]
     mod = __import__(module, globals(), locals(), [cname], 0)
     b = getattr(mod, cname).build_remotely(results)
-    b._storage_id = objid
+    b._storage_id = uuid.UUID(objid)
     return b
