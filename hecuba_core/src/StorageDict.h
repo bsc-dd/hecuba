c231f3091898797c67c3c4c4b06d22df0ed069f3
Pol Santamaria <polsm91@gmail.com>
2018-01-08 19:13:26 +0100

    Add new operations

--- hecuba_core/src/StorageDict.h
+++ hecuba_core/src/StorageDict.h
@@ -1,25 +1,69 @@
 #ifndef CPP_INTERFACE_STORAGEDICT_H
 #define CPP_INTERFACE_STORAGEDICT_H
 
-#include "ClusterConfig.h"
+
 #include <map>
 #include <iostream>
 //template <class E> class Bucket;
+#include "ClusterConfig.h"
+#include "TableMetadata.h"
+#include "TupleRow.h"
+#include "TupleRowFactory.h"
+#include "CacheTable.h"
 #include "Bucket.h"
 
 template<class K, class V>
 class StorageDict {
 public:
+    typedef std::pair<K,V> map_par;
+
+
+    StorageDict(std::initializer_list<std::pair<K,V>> l) {
+       for (const map_par* element= l.begin(); element!=l.end(); ++element) {
+           this->mymap[element->first] = new Bucket<K,V>(this,element->first);
+           this->mymap[element->first] = element->second;
+       };
+    }
 
     StorageDict(ClusterConfig *config) {
         this->cluster = config;
     }
 
-
-    ~StorageDict() {
-        delete(H);
+    StorageDict() {
+        this->cluster = nullptr;
     }
 
+    ~StorageDict() {
+        if (H) delete(H);
+        if (KeyFactory) delete(KeyFactory);
+        if (ValueFactory) delete(ValueFactory);
+        if (cluster) delete(cluster);
+    }
+
+
+    typedef std::pair<const K, V> value_type;
+    typedef typename std::map<K,Bucket<K,V> >::iterator iterator;
+    typedef typename std::map<K,Bucket<K,V> >::const_iterator const_iterator;
+
+
+    /* Modifiers */
+
+    std::pair<iterator,bool> insert (const value_type& val) {
+        iterator it = mymap.find(val.first);
+        if (it==mymap.end()) {
+            mymap[val.first]=new Bucket<K,V>(this,val.first);
+            mymap[val.first]=val.second;
+            return {mymap.find(val.first), false};
+        }
+        else {
+            return {mymap.find(val.first), true};
+        }
+
+    };
+
+
+
+    /* Operators */
 
     Bucket<K,V>& operator[]( const K& key ) {
         if (mymap.find(key)==mymap.end()) mymap[key] = new Bucket<K,V>(this,key);
@@ -43,6 +87,7 @@ public:
             mymap[k] = v;
         }
         if (H) this->store_data();
+        return *this;
     }
 
 
@@ -78,7 +123,7 @@ public:
 private:
 
     void store_data() {
-        for (typename std::map<K,Bucket<K,V>>::const_iterator it = mymap.begin(); it!=mymap.end(); ++it){
+        for (const_iterator it = mymap.begin(); it!=mymap.end(); ++it){
             K *k = new K(it->first);
             V *v = new V(it->second.getValue());
             TupleRow *key = KeyFactory->make_tuple(k);
@@ -89,14 +134,13 @@ private:
         }
     }
 
-    StorageDict() = default;
     //Writer *W = NULL;
-    CacheTable *H = NULL;
-    TupleRowFactory *KeyFactory = NULL;
-    TupleRowFactory *ValueFactory = NULL;
+    CacheTable *H = nullptr;
+    TupleRowFactory *KeyFactory = nullptr;
+    TupleRowFactory *ValueFactory = nullptr;
 
     std::map<K,Bucket<K,V> > mymap;
-    ClusterConfig *cluster;
+    ClusterConfig *cluster = nullptr;
 };
 
 
