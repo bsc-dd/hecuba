4b88e543c3f74bcceff8f8c162c9aabb621e7f7b
Pol Santamaria <polsm91@gmail.com>
2018-01-09 15:45:47 +0100

    Refactor and reformat

--- hfetch/Bucket.h
+++ hfetch/Bucket.h
@@ -1,13 +1,15 @@
 #ifndef HFETCH_BUCKET_H
 #define HFETCH_BUCKET_H
 
-template <class K,class V> class StorageDict;
+template<class K, class V>
+class StorageDict;
 
-template <class K, class V> class Bucket {
+template<class K, class V>
+class Bucket {
 private:
     V value;
     K key;
-    StorageDict<K,V> *sd = nullptr;
+    StorageDict<K, V> *sd = nullptr;
 
 public:
 
@@ -15,72 +17,74 @@ public:
 
     Bucket() = default;
 
-    Bucket(StorageDict<K,V>* storage, const K &key) {
+    Bucket(StorageDict<K, V> *storage, const K &key) {
         sd = storage;
         this->key = key;
 
     }
 
-    K getKey() const{
+    Bucket(StorageDict<K, V> *storage, const K &key, const V &value) {
+        sd = storage;
+        this->key = key;
+        this->value = value;
+
+    }
+
+    K getKey() const {
         return key;
     }
 
-    V getValue() const{
+    V getValue() const {
         return value;
     }
 
     /* COPY CONSTRUCTORS */
 
-    Bucket(const Bucket& b) {
-        this->key=b.getKey();
-        this->value=b.getValue();
+    Bucket(const Bucket &b) {
+        this->key = b.getKey();
+        this->value = b.getValue();
         this->sd = b.sd;
     }
 
 
-    Bucket(Bucket& b) {
-        this->key=b.getKey();
-        this->value=b.getValue();
+    Bucket(Bucket &b) {
+        this->key = b.getKey();
+        this->value = b.getValue();
         this->sd = b.sd;
     }
 
     /* OPERATORS */
 
     // copy assignment
-    Bucket& operator=(const V& other)
-    {
+    Bucket &operator=(const V &other) {
         this->value = other;
         if (sd) sd->raiseupd(this);
         return *this;
     }
 
     // copy assignment
-    Bucket& operator=(V& other)
-    {
+    Bucket &operator=(V &other) {
         this->value = other;
         if (sd) sd->raiseupd(this);
         return *this;
     }
 
     // copy assignment
-    Bucket& operator=(const V* other)
-    {
+    Bucket &operator=(const V *other) {
         this->value = *other;
         if (sd) sd->raiseupd(this);
         return *this;
     }
 
     // copy assignment
-    Bucket& operator=(V* other)
-    {
+    Bucket &operator=(V *other) {
         this->value = *other;
         if (sd) sd->raiseupd(this);
         return *this;
     }
 
     // data access pointer like
-    V operator->()
-    {
+    V operator->() {
         return this->value;
     }
 
@@ -91,35 +95,37 @@ public:
 
     // comparator
     bool operator==(const V &value) const {
-        return this->getValue()==value;
+        return this->getValue() == value;
     }
 
     // comparator
     bool operator<(const V &value) const {
-        return this->getValue()<value;
+        return this->getValue() < value;
     }
 
     // comparator
     bool operator>(const V &value) const {
-        return value<*this;
+        return value < *this;
     }
 
     // comparator
     bool operator<=(const V &value) const {
-        return !(value<*this);
+        return !(value < *this);
     }
+
     // comparator
     bool operator>=(const V &value) const {
-        return !(*this<value);
+        return !(*this < value);
     }
 
 
 };
 
 
-template<class K, class V> std::ostream& operator<<(std::ostream& os, const Bucket<K,V>& p)
-{
+template<class K, class V>
+std::ostream &operator<<(std::ostream &os, const Bucket<K, V> &p) {
     os << p.getValue();
     return os;
 }
+
 #endif //HFETCH_BUCKET_H
