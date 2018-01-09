228ef61148d5e3ce893474141dc9b997399e0be5
Pol Santamaria <polsm91@gmail.com>
2018-01-09 15:45:47 +0100

    Refactor and reformat

--- hecuba_core/src/StorageDict.h
+++ hecuba_core/src/StorageDict.h
@@ -14,17 +14,18 @@
 template<class K, class V>
 class StorageDict {
 public:
-    typedef std::pair<K,V> map_par;
+    typedef std::pair<K, V> value_type;
+    typedef std::pair<const K, V> value_type_c;
+    typedef typename std::map<K, Bucket<K, V> >::iterator iterator;
+    typedef typename std::map<K, Bucket<K, V> >::const_iterator const_iterator;
 
-
-    StorageDict(std::initializer_list<std::pair<K,V>> l) {
-       for (const map_par* element= l.begin(); element!=l.end(); ++element) {
-           this->mymap[element->first] = Bucket<K,V>(this,element->first);
-           this->mymap[element->first] = element->second;
-       };
+    StorageDict(std::initializer_list<std::pair<K, V>> l) {
+        for (const value_type *element = l.begin(); element != l.end(); ++element) {
+            this->mymap[element->first] = Bucket<K, V>(this, element->first, element->second);
+        };
     }
 
-    StorageDict(ClusterConfig *config) {
+    explicit StorageDict(ClusterConfig *config) {
         this->cluster = config;
     }
 
@@ -33,27 +34,20 @@ public:
     }
 
     ~StorageDict() {
-        if (H) delete(H);
-        if (KeyFactory) delete(KeyFactory);
-        if (ValueFactory) delete(ValueFactory);
+        if (H) delete (H);
+        if (KeyFactory) delete (KeyFactory);
+        if (ValueFactory) delete (ValueFactory);
     }
-
-
-    typedef std::pair<const K, V> value_type;
-    typedef typename std::map<K,Bucket<K,V> >::iterator iterator;
-    typedef typename std::map<K,Bucket<K,V> >::const_iterator const_iterator;
 
 
     /* Modifiers */
 
-    std::pair<iterator,bool> insert (const value_type& val) {
+    std::pair<iterator, bool> insert(const value_type_c &val) {
         iterator it = mymap.find(val.first);
-        if (it==mymap.end()) {
-            mymap[val.first]= Bucket<K,V>(this,val.first);
-            mymap[val.first]=val.second;
+        if (it == mymap.end()) {
+            mymap[val.first] = Bucket<K, V>(this, val.first, val.second);
             return {mymap.find(val.first), false};
-        }
-        else {
+        } else {
             return {mymap.find(val.first), true};
         }
 
@@ -63,26 +57,26 @@ public:
 
     /* Operators */
 
-    Bucket<K,V>& operator[]( const K& key ) {
-        if (mymap.find(key)==mymap.end()) mymap[key] =  Bucket<K,V>(this,key);
+    // Access
+    Bucket<K, V> &operator[](const K &key) {
+        if (mymap.find(key) == mymap.end()) mymap[key] = Bucket<K, V>(this, key);
+        return mymap[key];
+    };
+
+    // Access
+    Bucket<K, V> &operator[](K &&key) {
+        if (mymap.find(key) == mymap.end()) mymap[key] = Bucket<K, V>(this, key);
         return mymap[key];
     };
 
 
-    Bucket<K,V>& operator[]( K&& key ) {
-        if (mymap.find(key)==mymap.end()) mymap[key] =  Bucket<K,V>(this,key);
-        return mymap[key];
-    };
-
-
-
+    // Assignment
     StorageDict &operator=(std::map<K, V> somemap) {
         // Dirty method
-        for (typename std::map<K,V>::const_iterator it = somemap.begin(); it!=somemap.end(); ++it) {
-            K k = it->first;
-            V v = it->second;
-            mymap[k] =  Bucket<K,V>(this,k);
-            mymap[k] = v;
+        for (typename std::map<K, V>::const_iterator it = somemap.begin(); it != somemap.end(); ++it) {
+            K key = it->first;
+            V val = it->second;
+            mymap[key] = Bucket<K, V>(this, key, val);
         }
         if (H) this->store_data();
         return *this;
@@ -91,11 +85,12 @@ public:
 
     void make_persistent(std::string &name) {
         if (!cluster) throw ModuleException("StorageInterface not connected to any node");
-        std::vector<std::map<std::string, std::string>> keys_names = {{{"name","key"}}};;
-        std::vector<std::map<std::string, std::string>> columns_names = {{{"name","value"}}};
-        TableMetadata *table_meta = new TableMetadata(name.c_str(), "my_app", keys_names, columns_names, cluster->get_session());
-        std::map<std::string,std::string> config = {};
-        //W = new Writer(table_meta, cluster->get_session(), config);
+        std::vector<std::map<std::string, std::string>> keys_names = {{{"name", "key"}}};;
+        std::vector<std::map<std::string, std::string>> columns_names = {{{"name", "value"}}};
+        TableMetadata *table_meta = new TableMetadata(name.c_str(), "my_app", keys_names, columns_names,
+                                                      cluster->get_session());
+        std::map<std::string, std::string> config = {};
+
         H = new CacheTable(table_meta, cluster->get_session(), config);
         KeyFactory = new TupleRowFactory(table_meta->get_keys());
         ValueFactory = new TupleRowFactory(table_meta->get_values());
@@ -107,37 +102,33 @@ public:
     }
 
 
-
-    void raiseupd(Bucket<K,V> *b) {
+    void raiseupd(Bucket<K, V> *b) {
         if (H) {
             K *k = new K(b->getKey());
             V *v = new V(b->getValue());
             TupleRow *key = KeyFactory->make_tuple(k);
             TupleRow *value = ValueFactory->make_tuple(v);
             H->put_crow(key, value);
-            delete(key);
         }
     }
 
 private:
 
     void store_data() {
-        for (const_iterator it = mymap.begin(); it!=mymap.end(); ++it){
+        for (const_iterator it = mymap.begin(); it != mymap.end(); ++it) {
             K *k = new K(it->first);
             V *v = new V(it->second.getValue());
             TupleRow *key = KeyFactory->make_tuple(k);
             TupleRow *value = ValueFactory->make_tuple(v);
-            H->put_crow(key,value);
-            delete(key); 
+            H->put_crow(key, value);
         }
     }
 
-    //Writer *W = NULL;
     CacheTable *H = nullptr;
     TupleRowFactory *KeyFactory = nullptr;
     TupleRowFactory *ValueFactory = nullptr;
 
-    std::map<K,Bucket<K,V> > mymap;
+    std::map<K, Bucket<K, V> > mymap;
     ClusterConfig *cluster = nullptr;
 };
 
