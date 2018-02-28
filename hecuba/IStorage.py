f65815a006cb4bb02225b9c95e9b759794fec6e6
natacha <naqv1991@gmail.com>
2018-02-28 09:42:16 -0500

    Fixing delete persistent of every object. This will clean the objects from meomry

--- hecuba/IStorage.py
+++ hecuba/IStorage.py
@@ -196,8 +196,11 @@ class IStorage:
     def make_persistent(self, name):
         raise Exception("to be implemented")
 
-    def stop_persistent(self):
-        raise Exception("to be implemented")
+###############################################
+## This def will be removed                   #
+##############################################
+#    def stop_persistent(self):
+#        raise Exception("to be implemented")
 
     def delete_persistent(self):
         raise Exception("to be implemented")
