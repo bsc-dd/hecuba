f1add056913d7de7f47c126bdedbe75609fc3c77
Pol Santamaria <polsm91@gmail.com>
2018-08-16 20:44:25 +0200

    Add tests dependency

---
+++ hecuba_py/tests/app/words.py
@@ -0,0 +1,8 @@
+from hecuba.storageobj import StorageObj
+
+
+class Words(StorageObj):
+    '''
+    @ClassField words dict<<position:int>,wordinfo:str>
+    '''
+    pass
---
+++ hecuba_py/tests/withcassandra/app/result.py
@@ -0,0 +1,7 @@
+from hecuba.storageobj import StorageObj
+
+class Result(StorageObj):
+    '''
+    @ClassField instances dict<<word:str>,instances:int>
+    '''
+    pass
