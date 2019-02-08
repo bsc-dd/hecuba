dfd9ae941d6574010f15acca1aac0a6e0eae72a9
ismael <ismael.aik@gmail.com>
2019-02-08 12:44:38 +0100

    Fixed import error

--- hecuba_py/tests/withcassandra/__init__.py
+++
@@ -1,7 +0,0 @@
-import unittest
-loader = unittest.TestLoader()
-start_dir = '/home/bscuser/parser/hecuba/hecuba_py/tests/withcassandra'
-suite = loader.discover(start_dir)
-
-runner = unittest.TextTestRunner()
-runner.run(suite)
