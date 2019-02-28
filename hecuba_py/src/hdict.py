14b587f80af2983130bca35ba5858ccec599be17
Pol Santamaria <polsm91@gmail.com>
2019-02-28 16:04:41 +0100

    Make persistent saves the storage id

--- hecuba_py/src/hdict.py
+++ hecuba_py/src/hdict.py
@@ -565,10 +565,11 @@ class StorageDict(dict, IStorage):
 
         # Storing all in-memory values to cassandra
         for key, value in dict.iteritems(self):
-            if issubclass(value.__class__, IStorage) and not value._is_persistent:
+            if issubclass(value.__class__, IStorage):
                 # new name as ksp.table_valuename, where valuename is either defined by the user or set by hecuba
-                val_name = self._ksp + '.' + self._table + '_' + self._columns[0][0]
-                value.make_persistent(val_name)
+                if not value._is_persistent:
+                    val_name = self._ksp + '.' + self._table + '_' + self._columns[0][0]
+                    value.make_persistent(val_name)
                 value = value._storage_id
             self._hcache.put_row(self._make_key(key), self._make_value(value))
 
