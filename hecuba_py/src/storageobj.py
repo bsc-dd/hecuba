53b7bbd8f2b212a63d1ef3941c0526b4da47aca7
ismael <ismael.aik@gmail.com>
2019-04-08 12:27:13 +0200

    Build remotely doesnt create a new table

--- hecuba_py/src/storageobj.py
+++ hecuba_py/src/storageobj.py
@@ -49,7 +49,7 @@ class StorageObj(object, IStorage):
         parser = Parser("ClassField")
         return parser._parse_comments(comments)
 
-    def __init__(self, name="", tokens=None, storage_id=None, istorage_props=None, **kwargs):
+    def __init__(self, name="", tokens=None, storage_id=None, istorage_props=None, create_schema=True, **kwargs):
         """
             Creates a new storageobj.
             Args:
@@ -103,7 +103,8 @@ class StorageObj(object, IStorage):
 
         if self._is_persistent:
             # If never existed, must create the tables and register
-            self._create_tables()
+            if create_schema:
+                self._create_tables()
             self._store_meta(self._build_args)
 
         self._load_attributes()
