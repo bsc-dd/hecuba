32854112f97395ad97c699543b7b6c52c6a40b58
Ismael <ismael.aik@gmail.com>
2019-05-17 13:32:37 +0200

    Incremented the variable block_counter with the 'number of blocks in a cluster'

--- hecuba_core/src/SpaceFillingCurve.cpp
+++ hecuba_core/src/SpaceFillingCurve.cpp
@@ -274,12 +274,11 @@ int32_t ZorderCurveGenerator::computeNextClusterId() {
 
     std::vector<uint32_t> block_ccs = getIndexes(block_counter, blocks_dim);
     uint64_t zorder_id = computeZorder(block_ccs);
-    ++block_counter;
+    block_counter += (uint64_t) pow(CLUSTER_SIZE, 2);
     if (block_counter == nblocks) done = true;
     //Block parameters
     return (uint32_t) (zorder_id >> CLUSTER_SIZE);
 }
-
 
 /***
  * @param dims Dimensions of the future array
