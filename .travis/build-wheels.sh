f441d01e5d4f20dd7784a3a9fb99b07f41700a7e
cesare <cesare.cugnasco@gmail.com>
2019-05-22 11:50:40 +0200

    fixing building script

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 # Install a system package required by our library
 yum remove -y cmake
-yum install -y atlas-devel openssl openssl-devel cmake3 tbb-devel wget
+yum install -y atlas-devel openssl openssl-devel cmake3  wget
 ln /usr/bin/cmake3 /usr/bin/cmake
 
 # getting the latest libuv
@@ -21,16 +21,28 @@ cmake -H. -Bbuild
 cd build;make;make install
 
 
+# getting TBB
+cd;wget https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb2017_20161128oss_src.tgz
+tar -xf tbb2017_20161128oss_src.tgz
+cd tbb2017_20161128oss/
+make tbb
+cp -r build/*_release/libtbb.so*  /usr/lib64
+cp -r include/tbb /usr/include
+cd
+
 ORIGINAL_CPATH=${CPATH}
 ORIGINAL_LD=${LD_LIBRARY_PATH}
 export CFLAGS='-std=c++11'
 # Compile wheels
-for PYBIN in /opt/python/*; do
+for PYBIN in /opt/python/py27*; do
      VNAME=`basename ${PYBIN}`
      export CPATH=$PYBIN/include:${ORIGINAL_CPATH}
-     export LD_LIBRARY_PATH=${PYBIN}/lib:$PWD/build/lib:${ORIGINAL_LD}
+     export LD_LIBRARY_PATH=${PYBIN}/lib:/io/build/lib:${ORIGINAL_LD}
 
     "${PYBIN}/bin/pip" install -r /io/requirements.txt
+    cd /io
+    rm -rf build dist
+    "${PYBIN}/bin/python" setup.py install
     "${PYBIN}/bin/pip" wheel /io/ -w wheelhouse/${VNAME}
 
      # Bundle external shared libraries into the wheels
