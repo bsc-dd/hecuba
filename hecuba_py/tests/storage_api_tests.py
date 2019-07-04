4d74bbe41b74ba0d347804a20edf43573651581e
Pol Santamaria <polsm91@gmail.com>
2019-07-04 13:03:29 +0200

    Tests for PyCOMPSs storage api

--- hecuba_py/tests/storage_api_tests.py
+++ hecuba_py/tests/storage_api_tests.py
@@ -19,3 +19,15 @@ class StorageApi_Tests(unittest.TestCase):
 
         rebuild_dict = getByID(storage_id)
         self.assertTrue(isinstance(rebuild_dict, ApiTestSDict))
+
+
+    def object_id_is_str_test(self):
+
+        memory_obj = ApiTestSDict()
+
+        self.assertTrue(hasattr(memory_obj,'getID'))
+        self.assertIsInstance(memory_obj.getID(), str, "PyCOMPSs specs states that getID should return a string")
+
+        pers_dict = ApiTestSDict('test.api_id_str')
+        self.assertTrue(hasattr(pers_dict,'getID'))
+        self.assertIsInstance(pers_dict.getID(), str, "PyCOMPSs specs states that getID should return a string")
