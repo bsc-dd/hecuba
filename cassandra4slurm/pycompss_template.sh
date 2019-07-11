39bd5c57a019567092c2f28471821feda7223133
Eloy Gil <eloy@eloygil.com>
2019-07-11 15:53:28 +0200

    Update cassandra4slurm scripts

--- cassandra4slurm/pycompss_template.sh
+++ cassandra4slurm/pycompss_template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-module load boost/1.64.0_py2 COMPSs/2.2.rc1803
+#module load boost/1.64.0_py2 COMPSs/2.4.rc1903
+#module load COMPSs/2.4.rc1905
 if [ "${1}" != "-" ]; then
     interface=${1} # Receiving the interface, if any. (i.e. -ib0)
 fi
@@ -7,9 +8,9 @@ fi
 export CONTACT_NAMES=$(cat PLACEHOLDER_CASSANDRA_NODES_FILE | paste -s -d ' ' | sed "s/ /$interface,/g")$interface
 PYCOMPSS_NODES_FILE=PLACEHOLDER_PYCOMPSS_NODES_FILE
 echo "PYCOMPSS_NODES_FILE="$PYCOMPSS_NODES_FILE
-PYCOMPSS_MASTER=$(head -n 1 $PYCOMPSS_NODES_FILE | sed "s/$interface//")
+PYCOMPSS_MASTER=$(tail -n 1 $PYCOMPSS_NODES_FILE | sed "s/$interface//")
 echo "PYCOMPSS_MASTER is: "$PYCOMPSS_MASTER
-PYCOMPSS_WORKERS=$(tail -n +2 $PYCOMPSS_NODES_FILE | paste -s -d ' ' | sed "s/$interface//g")
+PYCOMPSS_WORKERS=$(head -n -1 $PYCOMPSS_NODES_FILE | paste -s -d ' ' | sed "s/$interface//g")
 #PYCOMPSS_WORKERS=$(cat $PYCOMPSS_NODES_FILE | paste -s -d ' ' | sed "s/$interface//g")
 echo "PYCOMPSS_WORKERS are: "$PYCOMPSS_WORKERS
 cmd=(launch_compss --master_node=$PYCOMPSS_MASTER --worker_nodes="$PYCOMPSS_WORKERS" --classpath=/home/bsc31/bsc31906/hecuba-src/storageLtf/StorageItf-1.0-jar-with-dependencies.jar --storage_conf=PLACEHOLDER_PYCOMPSS_STORAGE PLACEHOLDER_PYCOMPSS_FLAGS PLACEHOLDER_APP_PATH_AND_PARAMETERS)
