46f6be955f939a88c27aab39249e3bb29fddf1f2
Ismael <ismael.aik@gmail.com>
2019-12-04 12:32:32 +0100

    tests modifying a tuple

--- storage/cql_iface/tests/api_tests.py
+++ storage/cql_iface/tests/api_tests.py
@@ -1,8 +1,7 @@
 import decimal
-import time
 import unittest
 import uuid
-from typing import Tuple, FrozenSet, NamedTuple
+from typing import Tuple, NamedTuple
 
 import numpy
 
@@ -23,6 +22,7 @@ class TestClass(IStorage):
     def __init__(self, *args, **kwargs):
         super(TestClass, self).__init__()
 
+
 class TestClass2(IStorage):
 
     def __new__(cls, *args, name='', **kwargs):
@@ -31,6 +31,7 @@ class TestClass2(IStorage):
 
     def __init__(self, *args, **kwargs):
         super(TestClass2, self).__init__()
+
 
 class mockClass(IStorage):
     pass
@@ -576,7 +577,8 @@ class HfetchTests(unittest.TestCase):
 
         obj = TestClass(name=given_name)
         myid = obj.getID()
-        data_model = {"type": StorageDict, "value_id": {"k": int}, "fields": {"a": Tuple[int, int], "b": bool, "c": str}}
+        data_model = {"type": StorageDict, "value_id": {"k": int},
+                      "fields": {"a": Tuple[int, int], "b": bool, "c": str}}
         given_name = 'storage_test.dict'
         storage = CQLIface()
         data_model_id = storage.add_data_model(data_model)
@@ -605,7 +607,8 @@ class HfetchTests(unittest.TestCase):
 
         obj = TestClass(name=given_name)
         myid = obj.getID()
-        data_model = {"type": StorageDict, "value_id": {"k": int}, "fields": {"a": Tuple[int, int], "b": bool, "c": str, "d": int}}
+        data_model = {"type": StorageDict, "value_id": {"k": int},
+                      "fields": {"a": Tuple[int, int], "b": bool, "c": str, "d": int}}
         given_name = 'storage_test.dict'
         storage = CQLIface()
         data_model_id = storage.add_data_model(data_model)
@@ -634,7 +637,8 @@ class HfetchTests(unittest.TestCase):
 
         obj = TestClass(name=given_name)
         myid = obj.getID()
-        data_model = {"type": StorageDict, "value_id": {"k": int}, "fields": {"a": Tuple[int, int], "b": bool, "c": str, "d": int}}
+        data_model = {"type": StorageDict, "value_id": {"k": int},
+                      "fields": {"a": Tuple[int, int], "b": bool, "c": str, "d": int}}
         given_name = 'storage_test.dict'
         storage = CQLIface()
         data_model_id = storage.add_data_model(data_model)
@@ -671,5 +675,48 @@ class HfetchTests(unittest.TestCase):
 
         for val, ret_val in zip(fields1.values(), returned_values[0]):
             self.assertEqual(val, ret_val)
+
+    def test_put_record_StorageDict_complex_less_values_2_inserts_tuple_modif(self):
+        given_name = 'storage_test.complex_obj'
+        config.session.execute("DROP TABLE IF EXISTS {}".format(given_name))
+
+        obj = TestClass(name=given_name)
+        myid = obj.getID()
+        data_model = {"type": StorageDict, "value_id": {"k": int},
+                      "fields": {"a": Tuple[int, int], "b": bool, "c": str, "d": int}}
+        given_name = 'storage_test.dict'
+        storage = CQLIface()
+        data_model_id = storage.add_data_model(data_model)
+        storage.register_persistent_object(data_model_id, obj)
+
+        keys = NamedTuple('keys', [('k', int)])
+        keys = keys(8)._asdict()
+        fields = NamedTuple('fields', [('a', Tuple[int, int]), ('b', bool), ('c', str)])
+        fields1 = fields((3, None), True, 'adeu')._asdict()
+
+        storage.put_record(myid, keys, fields1)
+        returned_values = []
+        hcache = storage.hcache_by_id[myid]
+        for key in keys.values():
+            returned_values.append(hcache.get_row([key]))
+
+        self.assertEqual(len(data_model["fields"]), len(returned_values[0]))
+
+        for val, ret_val in zip(fields1.values(), returned_values[0]):
+            self.assertEqual(val, ret_val)
+        fields = NamedTuple('fields', [('a', Tuple[int, int])])
+        fields = fields((3, 4))._asdict()
+
+        storage.put_record(myid, keys, fields)
+
+        returned_values = []
+        hcache = storage.hcache_by_id[myid]
+        for key in keys.values():
+            returned_values.append(hcache.get_row([key]))
+
+        self.assertEqual(len(data_model["fields"]), len(returned_values[0]))
+        self.assertEqual([(3, 4), True, 'adeu', None], returned_values[0])
+
+
 if __name__ == "__main__":
     unittest.main()
