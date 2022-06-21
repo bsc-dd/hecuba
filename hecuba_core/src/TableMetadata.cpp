898e2f4f7b5b4ffc7f44e83f20cd4749532ad4d3
Juanjo Costa <jcosta@ac.upc.edu>
2022-06-21 15:51:29 +0200

    Add get_single_column to TableMetadata
        * Extract the ColumnMeta from a column name.

--- hecuba_core/src/TableMetadata.cpp
+++ hecuba_core/src/TableMetadata.cpp
@@ -402,6 +402,16 @@ std::shared_ptr<const std::vector<ColumnMeta> > TableMetadata::get_single_value(
     throw ModuleException("get_single_value: Unknown column name [" + value + "]");
 }
 
+const ColumnMeta *TableMetadata::get_single_column(const std::string &value) const {
+    // TODO : Add a hash map to cache 'value_name' ColumnMeta and avoid searching
+    int pos = get_columnname_position(value);
+    if (pos>=0) {
+        const ColumnMeta *m = &(*cols)[pos];
+        return m;
+    }
+    throw ModuleException("get_single_value: Unknown column name [" + value + "]");
+}
+
 // completes the build of the insert query for just one attribute
 const char *TableMetadata::get_partial_insert_query(const std::string &attr_name) const {
     uint32_t n_keys = (uint32_t) keys->size();
