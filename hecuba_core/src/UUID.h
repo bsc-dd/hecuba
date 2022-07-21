b7c645af365858865e9963646eddf8c63e686fd4
Juanjo Costa <jcosta@ac.upc.edu>
2022-07-21 12:46:29 +0200

    Add UUID code
        * Create a functional unit to tread UUID: generation, to_string

---
+++ hecuba_core/src/UUID.h
@@ -0,0 +1,13 @@
+#ifndef UUID_H
+#define UUID_H
+
+#include <cstdint>
+#include <string>
+
+namespace UUID {
+    uint64_t* generateUUID(void) ;
+    uint64_t* generateUUID5(const char* name) ;
+    std::string UUID2str(uint64_t* c_uuid);
+}
+
+#endif /* UUID_H */
