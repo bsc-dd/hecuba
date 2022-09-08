dac5d833a491e53b4bf4932e107bc0a8344918ef
yolandab <yolandab@ac.upc.edu>
2022-09-08 19:30:51 +0200

    Hide empty messages when killing processes
        * There was a message saying that it was unable to kill
          cassandra/kafka/zookeeper processes in a node. These messages were
          harmless as the script was being executed in all nodes in the
          cluster.

--- cassandra4slurm/scripts/killer.sh
+++ cassandra4slurm/scripts/killer.sh
@@ -14,30 +14,30 @@
 ###############################################################################################################
 
 # Kill Cassandra instance
-PID=$(ps aux | grep java | grep cassandra | awk '{ print $2 }')
+PID=$(ps aux | grep java | grep -v grep| grep cassandra | awk '{ print $2 }')
 if [ "$PID" != "" ]; then
     echo "Host $(hostname) is killing its cassandra process with PID: "$PID
     kill $PID
-else
-    echo "Host $(hostname) can not find a running cassandra process to kill."
 fi
 
 # Kill Arrow Helpers
-PID=$(ps aux |grep arrow_helper | awk '{print $2}')
+PID=$(ps aux |grep arrow_helper | grep -v grep| awk '{print $2}')
 if [ "$PID" != "" ]; then
     echo "Host $(hostname) is killing its arrow_helper process with PID: "$PID
     kill $PID
-else
-    echo "Host $(hostname) can not find a running arrow_helper process to kill."
 fi
 
 # Kill Kafka
-PID=$(ps aux |grep java | awk '{print $2}')
+PID=$(ps aux |grep java | grep -v grep| grep kafka | awk '{print $2}')
 if [ "$PID" != "" ]; then
     echo "Host $(hostname) is killing its kafka process with PID: "$PID
     kill $PID
-else
-    echo "Host $(hostname) can not find a running kafka process to kill."
+fi
+
+PID=$(ps aux |grep java | grep -v grep| grep zookeeper | awk '{print $2}')
+if [ "$PID" != "" ]; then
+    echo "Host $(hostname) is killing its zookeeper process with PID: "$PID
+    kill $PID
 fi
 
 exit
