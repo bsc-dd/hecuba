131f57be7d933b91cf1c911069d53707b532ee7e
Juanjo Costa <jcosta@ac.upc.edu>
2022-09-14 09:29:29 +0200

    Allow UUID2str to accept null UUIDs
        * Instead of faulting.

--- hecuba_core/src/UUID.h
+++ hecuba_core/src/UUID.h
@@ -7,7 +7,7 @@
 namespace UUID {
     uint64_t* generateUUID(void) ;
     uint64_t* generateUUID5(const char* name) ;
-    std::string UUID2str(uint64_t* c_uuid);
+    std::string UUID2str(const uint64_t* c_uuid);
 }
 
 #endif /* UUID_H */
