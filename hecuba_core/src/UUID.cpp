9597c6de3b47e52521a9f5517f248563da5c3121
Juanjo Costa <jcosta@ac.upc.edu>
2022-09-14 09:29:29 +0200

    Allow UUID2str to accept null UUIDs
        * Instead of faulting.

--- hecuba_core/src/UUID.cpp
+++ hecuba_core/src/UUID.cpp
@@ -37,19 +37,23 @@ uint64_t* UUID::generateUUID5(const char* name) {
     return c_uuid;
 }
 
-std::string UUID::UUID2str(uint64_t* c_uuid) {
+std::string UUID::UUID2str(const uint64_t* c_uuid) {
     /* This MUST match with the 'cass_statement_bind_uuid' result */
     char str[37] = {};
-    unsigned char* uuid = reinterpret_cast<unsigned char*>(c_uuid);
+    const unsigned char* uuid = reinterpret_cast<const unsigned char*>(c_uuid);
     //std::cout<< "HecubaSession: uuid2str: BEGIN "<<std::hex<<c_uuid[0]<<c_uuid[1]<<std::endl;
-    sprintf(str,
-        "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-        uuid[0], uuid[1], uuid[2], uuid[3],
-        uuid[4], uuid[5],
-        uuid[6], uuid[7],
-        uuid[8], uuid[9],
-        uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]
-        );
+    if (c_uuid == nullptr) {
+        sprintf(str, "(null)");
+    } else {
+        sprintf(str,
+            "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+            uuid[0], uuid[1], uuid[2], uuid[3],
+            uuid[4], uuid[5],
+            uuid[6], uuid[7],
+            uuid[8], uuid[9],
+            uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]
+            );
+    }
     //std::cout<< "HecubaSession: uuid2str: "<<str<<std::endl;
     return std::string(str);
 }
