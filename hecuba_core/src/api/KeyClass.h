272a6896f638a940e7950d160426104d2cf4209d
yolandab <yolanda.becerra@bsc.es>
2022-12-22 09:53:43 +0100

    Add the class AttributeClass
    	* Encapsulate the common code for keys and values in AttributeClass
    	* Enable getting attributes from key classes

--- hecuba_core/src/api/KeyClass.h
+++ hecuba_core/src/api/KeyClass.h
@@ -7,139 +7,71 @@
 #include <vector>
 #include <hecuba/debug.h>
 #include <hecuba/ObjSpec.h>
+#include <hecuba/ObjSpec.h>
+#include "AttributeClass.h"
 
 
 template <class K1, class...rest>
 
-class KeyClass {
+class KeyClass:public AttributeClass<K1,rest...> {
     //First _KeyClass;
     //KeyClass<Rest...> _nextKeyClass;
 public:
     KeyClass() = default;
 
     // Constructor without specifying names.
-    KeyClass(K1 part, rest... clustering) {
-        //DEBUG("Number of clustering keys: "<<sizeof...(rest)<<std::endl);
-        // first element is the partition key, the rest of elements are clustering keys
-        // only basic classes are suported
-        manageKey<K1>(part);
-        manageClustering(clustering...);
-	createKeysBuffer();
+    KeyClass(K1 part, rest... clustering):AttributeClass<K1,rest...>("keyname",part,clustering...) {
+	setKeys(); // the first one of the attributes is the partition key, and the rest the clustering keys
     }
 
-    KeyClass(const KeyClass &w){
-        partitionKey=w.partitionKey;
-	partitionKeys=w.partitionKeys;
-	clusteringKeys=w.clusteringKeys;
+    KeyClass(const KeyClass &w):AttributeClass<K1,rest...>(){
+	this->partitionKeys=w.partitionKeys;
+	this->clusteringKeys=w.clusteringKeys;
+	this->total_size=w.total_size;
+	this->valuesBuffer = (char *) malloc (this->total_size);
+	memcpy(this->valuesBuffer, w.valuesBuffer,this->total_size);
     }
 
+    // copy assignment
     void operator=(const KeyClass &w) {
-        partitionKey=w.partitionKey;
-	partitionKeys=w.partitionKeys;
-	clusteringKeys=w.clusteringKeys;
+	this->partitionKeys=w.partitionKeys;
+	this->clusteringKeys=w.clusteringKeys;
+	this->total_size=w.total_size;
+	this->valuesBuffer = (char *) malloc (this->total_size);
+	memcpy(this->valuesBuffer, w.valuesBuffer,this->total_size);
     }
-
-    template <class K> void manageKey(K key) {
-            //DEBUG("Key " << key << std::endl);
-	    std::string keytype=ObjSpec::c_to_cass(typeid(decltype(key)).name());
-	    std::pair<std::string, std::string> keydesc("keyname"+std::to_string(managedKeys), keytype);
-	    partitionKeys.push_back(keydesc);
-
-	    createAttributeBuffer<K>(keytype, key);
-    	    managedKeys++;
-            partitionKey = key;
-    }
-
-    
-
-    template <class K1alt, class...restalt> void manageClustering(K1alt part, restalt... clustering){
-        /* deal with the first parameter */
-        //DEBUG("Clust Key " << part << " "<<std::string(typeid(decltype(part)).name())<<std::endl);
-	// TODO convert c++ type to cass type
-
-	std::string keytype=ObjSpec::c_to_cass(typeid(decltype(part)).name());
-	std::pair<std::string, std::string> keydesc("keyname"+std::to_string(managedKeys), keytype);
-	clusteringKeys.push_back(keydesc);
-	createAttributeBuffer<K1alt>(keytype, part);
-    	managedKeys++;
-        manageClustering(clustering...);
-    }
-
-    void manageClustering() {
-    }
-
-
-    template <class K> void createAttributeBuffer(std::string keytype, K Key) {
-	    char * buf;
-            int size=0;
-            if (ObjSpec::isBasicType(keytype)) {
-		if (typeid(K) == typeid(std::string)) {
-			//copy the string so we can pass the address of the copy
-			std::string keystring = reinterpret_cast<std::string&>(Key);
-			char *keytmp=(char *)keystring.c_str();
-			char *copyKey= (char *) malloc(strlen(keytmp) + 1);
-			strcpy(copyKey, keytmp);
-			size=sizeof(copyKey);
-			buf = (char *) malloc(size);
-			memcpy(buf, &copyKey, size);
-		} else {
-                	size=sizeof(Key);
-                	buf = (char *) malloc(size);
-                	memcpy(buf, &Key, size);
-		}
-            } else {
-                char * Kpointer = (char *) &Key;
-                size=sizeof(Kpointer);
-                buf = (char *) malloc(size);
-                memcpy(buf, &Kpointer, size);
-            }
-            std::pair <char *, int> bufinfo(buf,size);
-            keysTmpBuffer.push_back(bufinfo);
-            total_size += size;
-    }
-
-    void createKeysBuffer() {
-	keysBuffer = (char *) malloc(total_size);
-	char *pBuffer = keysBuffer;
-	for (std::vector <std::pair<char *, int>>::iterator it=keysTmpBuffer.begin(); it!=keysTmpBuffer.end(); it++) {
-		memcpy(pBuffer, it->first, it->second);
-		pBuffer+=it->second;
-	}
-    }
-
 
     K1 getPartitionKey() {
         //DEBUG("Partition key "<< partitionKey << std::endl);
-        return partitionKey;             
+        return this->partitionKey;             
     }
-    void  generateKeyDescr() {
-	if (managedKeys==0) {
-		std::vector<const char *> v={typeid(K1).name(),typeid(rest).name()...};
-		std::pair<std::string, std::string> keydesc("keyname"+std::to_string(managedKeys), ObjSpec::c_to_cass(v[0]));
-    		managedKeys++;
-		partitionKeys.push_back(keydesc);
-		std::vector<const char *>::iterator it = v.begin();
-    		it++;
-		while (it != v.end()) {
-			std::pair <std::string,std::string> keydesc ("keyname"+std::to_string(managedKeys), ObjSpec::c_to_cass(*it));
-			clusteringKeys.push_back(keydesc);
-			managedKeys++;
-			++it;
-		}
-	}
-    }
+
     std::vector<std::pair<std::string, std::string>> getPartitionKeys() {
-	if (managedKeys == 0) {
-		generateKeyDescr();
+	if (this->managedValues == 0) {
+		this->generateAttrDescr("keyname");
+		setKeys();
 	}
-	return partitionKeys;
+	return this->partitionKeys;
     }
     std::vector<std::pair<std::string, std::string>> getClusteringKeys() {
-	if (managedKeys == 0) {
-		generateKeyDescr();
+	if (this->managedValues == 0) {
+		this->generateAttrDescr("keyname");
+		setKeys();
 	}
-	return clusteringKeys;
+	return this->clusteringKeys;
+    }
 
+    void setKeys() {
+	 std::vector<std::pair<std::string,std::string >>::iterator it = this->valuesDesc.begin();
+         while (it != this->valuesDesc.end()) {
+		if (it == this->valuesDesc.begin()) {
+                	this->partitionKeys.push_back(*it);
+		} else {
+                	this->clusteringKeys.push_back(*it);
+		}
+                this->managedValues++;
+                ++it;
+         }
     }
 
     bool operator==(const KeyClass &k) const {
@@ -165,23 +97,15 @@ public:
     }
 
     char *getKeysBuffer() {
-	return keysBuffer;
+	return this->getValuesBuffer();
     }
     int getTotalSize() {
-	return total_size;
+	return this->total_size;
     }
 
-    K1 partitionKey; 
     std::vector<std::pair<std::string, std::string>> partitionKeys;
     std::vector<std::pair<std::string, std::string>> clusteringKeys;
 
-    std::vector <std::pair<char *, int>> keysTmpBuffer;
-    int total_size = 0;
-
-    char *keysBuffer;
-
-
-    int32_t managedKeys=0;
 
 
 };
--- hecuba_core/src/api/ValueClass.h
+++ hecuba_core/src/api/ValueClass.h
@@ -9,12 +9,12 @@
 
 #include <hecuba/debug.h>
 #include "IStorage.h"
+#include "AttributeClass.h"
 
 
 
 template <class V1, class...rest>
-
-class ValueClass {
+class ValueClass:public AttributeClass<V1,rest...>{ 
     //First _KeyClass;
     //KeyClass<Rest...> _nextKeyClass;
 public:
@@ -22,51 +22,45 @@ public:
     ValueClass() =default; 
 
     // Constructor called when instantiating a new value with parameters: MyValueClass v(value);
-   ValueClass(V1 part, rest... vals) {
-        //DEBUG("Number of clustering keys: "<<sizeof...(values)<<std::endl);
-        // first element is the partition key, the rest of elements are clustering keys
-        // only basic classes are suported
-        manageValue<V1>(part);
-        manageRest(vals...);
-	createValuesBuffer();
-	values=std::make_tuple(part, vals...);
+   ValueClass(V1 part, rest... vals):AttributeClass<V1,rest...>("valuename",part,vals...) {
    }
 
     // Constructor called when assigning another value during the instatiation: MyValueClass v = otherValue or MyValueClass v = d[k]
-    ValueClass(const ValueClass &w){
-        valuesDesc=w.valuesDesc;
-	sd = w.sd;
+    ValueClass(const ValueClass &w): AttributeClass<V1,rest...>(){
+        this->valuesDesc=w.valuesDesc;
+	this->sd = w.sd;
 	
 	if (w.pendingKeysBuffer != nullptr) {
 		// case myvalueclass v =d[k]
 		std::cout << "GetItem: move assignemnt to reference to object" << std::endl;
 		//valuesBuffer= (char *)malloc(total_size);
-		total_size=sd->getDataWriter()->get_metadata()->get_values_size();
-		managedValues = w.managedValues;
-		if (managedValues == 1) {
-			valuesBuffer= (char *)malloc(total_size);
-			sd->getItem(w.pendingKeysBuffer,valuesBuffer);
+		this->total_size=this->sd->getDataWriter()->get_metadata()->get_values_size();
+		this->managedValues = w.managedValues;
+		if (this->managedValues == 1) {
+			this->valuesBuffer= (char *)malloc(this->total_size);
+			this->sd->getItem(w.pendingKeysBuffer,this->valuesBuffer);
 		} else {
 			// if multivalue, getitem allocates the space
-			sd->getItem(w.pendingKeysBuffer,&valuesBuffer);
+			this->sd->getItem(w.pendingKeysBuffer,&this->valuesBuffer);
 		}
 		free(w.pendingKeysBuffer);
-		pendingKeysBuffer = nullptr;
-		pendingKeysBufferSize=0;
-		setTupleValues<0,V1,rest...>(valuesBuffer);
+		this->pendingKeysBuffer = nullptr;
+		this->pendingKeysBufferSize=0;
+		//this->setTupleValues<0,V1,rest...>(this->valuesBuffer);
+		this->template setTupleValues<0,V1,rest...>(this->valuesBuffer);
 	} else {
 		// case myvalueclass v = j copy constructor
-    		managedValues = w.managedValues;
-		total_size = w.total_size;
-		valuesBuffer = (char *) malloc(total_size);
-		memcpy(valuesBuffer, w.valuesBuffer, total_size);
-		values = w.values;
+    		this->managedValues = w.managedValues;
+		this->total_size = w.total_size;
+		this->valuesBuffer = (char *) malloc(this->total_size);
+		memcpy(this->valuesBuffer, w.valuesBuffer, this->total_size);
+		this->values = w.values;
 	}
 
     }
 
    //Constructor called by the operator [] of StorageDict
-    ValueClass(IStorage *sd,char *keysBuffer, int bufferSize) {
+    ValueClass(IStorage *sd,char *keysBuffer, int bufferSize):AttributeClass<V1,rest...>() {
 	this->sd = sd;
 	this->pendingKeysBuffer = (char *) malloc(bufferSize);
 	this->pendingKeysBufferSize = bufferSize;
@@ -79,24 +73,24 @@ public:
     ValueClass &operator = (ValueClass & w) {
 	//  case v=sd[k]
 	if (w.pendingKeysBuffer != nullptr) {
-		sd=w.sd;
-		total_size=w.total_size;
+		this->sd=w.sd;
+		this->total_size=w.total_size;
 		std::cout << "GetItem: move assignemnt to reference to object" << std::endl;
-		managedValues = w.managedValues;
-		if (managedValues == 1) {
-			valuesBuffer= (char *)malloc(total_size);
-			sd->getItem(w.pendingKeysBuffer,valuesBuffer);
+		this->managedValues = w.managedValues;
+		if (this->managedValues == 1) {
+			this->valuesBuffer= (char *)malloc(this->total_size);
+			this->sd->getItem(w.pendingKeysBuffer,this->valuesBuffer);
 		} else {
 			// if multivalue, getitem allocates the space
-			sd->getItem(w.pendingKeysBuffer,&valuesBuffer);
+			this->sd->getItem(w.pendingKeysBuffer,&this->valuesBuffer);
 		}
 		free(w.pendingKeysBuffer);
-		pendingKeysBuffer = nullptr;
+		this->pendingKeysBuffer = nullptr;
 		w.pendingKeysBuffer=nullptr;
-		pendingKeysBufferSize=0;
+		this->pendingKeysBufferSize=0;
 		w.pendingKeysBufferSize=0;
-        	valuesDesc=w.valuesDesc;
-		setTupleValues<0,V1,rest...>(valuesBuffer);
+        	this->valuesDesc=w.valuesDesc;
+		this->template setTupleValues<0,V1,rest...>(this->valuesBuffer);
 		// TODO set values interpreting valuesBuffer
 		w.sd=nullptr;
 		w.managedValues=0;
@@ -104,129 +98,21 @@ public:
 	} else {
 		//  case sd[k]=v;
 		std::cout << "SetItem: copy assignemnt to reference to object" << std::endl;
-		if (pendingKeysBuffer != nullptr) {
-			sd->setItem(pendingKeysBuffer,w.valuesBuffer);
-			free(pendingKeysBuffer);
-			pendingKeysBuffer = nullptr;
-			pendingKeysBufferSize=0;
-        		valuesDesc=w.valuesDesc;
-    			managedValues = w.managedValues;
-			valuesBuffer = w.valuesBuffer;
-			total_size = w.total_size;
-			values = w.values;
+		if (this->pendingKeysBuffer != nullptr) {
+			this->sd->setItem(this->pendingKeysBuffer,w.valuesBuffer);
+			free(this->pendingKeysBuffer);
+			this->pendingKeysBuffer = nullptr;
+			this->pendingKeysBufferSize=0;
+        		this->valuesDesc=w.valuesDesc;
+    			this->managedValues = w.managedValues;
+			this->valuesBuffer = w.valuesBuffer;
+			this->total_size = w.total_size;
+			this->values = w.values;
 		} 
 	}
 
 	return *this;
 
-    }
-
-    template <class V> void manageValue(V value) {
-            //DEBUG("Key " << key << std::endl);
-	    std::string valuetype=ObjSpec::c_to_cass(typeid(decltype(valuetype)).name());
-	    std::pair<std::string, std::string> valuedesc("valuename"+std::to_string(managedValues), valuetype);
-	    valuesDesc.push_back(valuedesc);
-	    createAttributeBuffer <V>(valuetype,value);
-    	    managedValues++;
-    }
-
-    template <class V1alt, class...restalt> void manageRest(V1alt part, restalt... restValues){
-        /* deal with the first parameter */
-        //DEBUG("Clust Key " << part << " "<<std::string(typeid(decltype(part)).name())<<std::endl);
-	std::string valuetype=ObjSpec::c_to_cass(typeid(decltype(part)).name());
-	std::pair<std::string, std::string> valuedesc("valuename"+std::to_string(managedValues), valuetype);
-	valuesDesc.push_back(valuedesc);
-	createAttributeBuffer <V1alt>(valuetype,part);
-    	managedValues++;
-        manageRest(restValues...);
-    }
-
-    void manageRest() {
-    }
-
-    template <std::size_t ix, class V1alt, class...restalt> void setTupleValues(void *buffer) {
-
-	size_t tam = 0;
-	if (ObjSpec::isBasicType(valuesDesc[ix].second) ) {
-		if (valuesDesc[ix].second == "text") {
-			(std::string &&)std::get<ix>(values)=std::string(*(char **)buffer);
-			tam = sizeof(char *);
-		} else {
-			std::get<ix>(values)=*(V1alt *)buffer;
-			tam = sizeof (V1alt);
-		}
-	} else {
-		std::get<ix>(values)=*(V1alt *)buffer;
-		tam = sizeof(V1alt);
-	}
-	
-	setTupleValues<ix+1, restalt...>((void *)((char*)buffer+tam));
-    }
-
-    template <std::size_t ix> void setTupleValues(void *buffer) {
-
-    }
-
-
-       template <class V> void createAttributeBuffer(std::string valuetype, V value) {
-            char * buf;
-            int size=0;
-            if (ObjSpec::isBasicType(valuetype)) {
-
-		if (typeid(value) == typeid(std::string)){
-                        //copy the string so we can pass the address of the copy
-			std::string valuestring = reinterpret_cast<std::string&>(value);
-                        const char *valuetmp=valuestring.c_str();
-                        char *copyValue=(char *)malloc(strlen(valuetmp) + 1);
-                        strcpy(copyValue, valuetmp);
-			size = sizeof(copyValue);
-                        buf = (char *) malloc(size);
-                        memcpy(buf, &copyValue, size);
-                } else {
-			size=sizeof(value);
-                	buf = (char *) malloc(size);
-                	memcpy(buf, &value, size);
-		}
-
-            } else {
-                char * Vpointer = (char *) &value;
-                size=sizeof(Vpointer);
-                buf = (char *) malloc(size);
-                memcpy(buf, &Vpointer, size);
-            }
-            std::pair <char *, int> bufinfo(buf,size);
-            valuesTmpBuffer.push_back(bufinfo);
-            total_size += size;
-    }
-
-    void createValuesBuffer() {
-        valuesBuffer = (char *) malloc(total_size);
-        char *pBuffer = valuesBuffer;
-        for (std::vector <std::pair<char *, int>>::iterator it=valuesTmpBuffer.begin(); it!=valuesTmpBuffer.end(); it++) {
-                memcpy(pBuffer, it->first, it->second);
-                pBuffer+=it->second;
-        }
-    } 
-
-    void generateValueDescr() {
-	if (managedValues == 0) {
-                std::vector<const char *> v={typeid(V1).name(),typeid(rest).name()...};
-                std::vector<const char *>::iterator it = v.begin();
-                while (it != v.end()) {
-                        std::pair <std::string,std::string> valuedesc ("valuename"+std::to_string(managedValues), ObjSpec::c_to_cass(*it));
-                        valuesDesc.push_back(valuedesc);
-                        managedValues++;
-                        ++it;
-                }
-	}
-
-    }
-
-    std::vector<std::pair<std::string, std::string>> getValuesDesc() {
-	if (managedValues == 0) {
-		generateValueDescr();
-	}
-	return valuesDesc;
     }
 
     bool operator==(const ValueClass &v) const {
@@ -250,31 +136,6 @@ public:
     bool operator>=(const ValueClass &value) const {
         return true;
     }
-
-    std::vector<std::pair<std::string, std::string>> valuesDesc;
-    int32_t managedValues=0;
-    std::vector <std::pair<char *, int>> valuesTmpBuffer;
-    int total_size = 0;
-    IStorage *sd = nullptr;
-    char *pendingKeysBuffer=nullptr;
-    int pendingKeysBufferSize=0;
-    char *valuesBuffer = nullptr;
-
-    char *getValuesBuffer() {
-	return valuesBuffer;
-    }
-    void setValuesBuffer(char *valuesBuffer) {
-	this->valuesBuffer = valuesBuffer;
-    }
-    void setTotalSize(int size) {
-	this->total_size = size;
-    }
-
-std::tuple <V1, rest...> values;
-
-template <std::size_t ix, class ...Types> static typename std::tuple_element<ix, std::tuple<Types...>>::type& get(ValueClass<Types...>&v) {
-	return std::get<ix>(v.values);
-}
 
 };
 
