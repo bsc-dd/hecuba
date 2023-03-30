a51105f342a847c1c2db47b24ebfac0b5a1fbd14
yolandab <yolanda.becerra@bsc.es>
2023-03-30 12:55:09 +0200

    Clean out commented code.

--- hecuba_core/src/api/SORecursiveMagic.h
+++ hecuba_core/src/api/SORecursiveMagic.h
@@ -20,10 +20,6 @@
 #define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
 #define EVAL1(...) __VA_ARGS__
 
-#define EMPTY()
-#define DEFER1(m) m EMPTY()
-#define DEFER2(m) m EMPTY EMPTY()()
-
 #define FIRST(a, ...) a
 #define SECOND(a, b, ...) b
 
@@ -49,89 +45,29 @@
 #define _IF_1_ELSE(...)
 #define _IF_0_ELSE(...) __VA_ARGS__
 
-//#define MAP(m, first,second, ...)           \
-        m(first,second)                           \
-        IF_ELSE(HAS_ARGS(__VA_ARGS__))(    \
-              DEFER2(_MAP)()(m, __VA_ARGS__)   \
-        )(                                 \
-                    /* Do nothing, just terminate */ \
-        )
-
-//#define MAP_1par(m, first, ...)           \
-        m(first)                           \
-        IF_ELSE(HAS_ARGS(__VA_ARGS__))(    \
-              DEFER2(_MAP_1par)()(m, __VA_ARGS__)   \
-        )(                                 \
-                    /* Do nothing, just terminate */ \
-        )
-//#define _MAP_1par() MAP_1par
-
-// #define _MAP() MAP
-
-//#define TOFIELD(t1,n1) FIELD(t1,n1)
-
-//#define FIELD(t1,n1) t1 n1;
-
-#define VECTOR(t1,n1, ...) \
-    {typeid(t1).name(), std::string(#n1)} \
-    IF_ELSE(HAS_ARGS(__VA_ARGS__))( \
-            , DEFER2(_VECTOR)()(__VA_ARGS__)\
-    ) ( )
-
-#define _VECTOR() VECTOR
-
-//#define DECLARE(t1,n1,...) \
-    EVAL(MAP(FIELD,t1,n1,__VA_ARGS__)) \
-    std::vector<std::pair<std::string,std::string>>c_types_attr={ EVAL(VECTOR(t1,n1,__VA_ARGS__)) };\
-    std::vector<std::pair<std::string, std::string>> get_attributes_info() {return c_types_attr;}; 
-    
-#if 0
-#define GEN_STRUCT(t1,n1, ...) \
-    &n1 \
-    IF_ELSE(HAS_ARGS(__VA_ARGS__))( \
-          , DEFER2(_GEN_STRUCT)()(__VA_ARGS__)\
-    ) ( )
-#define _GEN_STRUCT() GEN_STRUCT
-#endif
-
 #define DECL(t1,n1, ...) \
     SO_Attribute<t1> n1={this,#n1}; \
     IF_ELSE(HAS_ARGS(__VA_ARGS__))( \
            DEFER2(_DECL)()(__VA_ARGS__)\
     ) ( )
-#define _DECL() DECL 
+#define _DECL() DECL
 
-    // it is not necessary to pass the address of the attributes
-    //#define DEFAULT_CONSTRUCTOR(clase, t1,n1,...)  EVAL(DECL(t1,n1,__VA_ARGS__)) clase():StorageObject({ EVAL(VECTOR(t1,n1,__VA_ARGS__))},{EVAL(GEN_STRUCT(t1,n1,__VA_ARGS__))})
-
-#define DEFAULT_CONSTRUCTOR(clase, t1,n1,...)  EVAL(DECL(t1,n1,__VA_ARGS__)) clase():StorageObject({ EVAL(VECTOR(t1,n1,__VA_ARGS__))})
-
-// it is not necessary to pass the address of the attributes
-//#define SO_CONSTRUCTOR(t1,n1,...) StorageObject({ EVAL(VECTOR(t1,n1,__VA_ARGS__))},{EVAL(GEN_STRUCT(t1,n1,__VA_ARGS__))})
-#define SO_CONSTRUCTOR(t1,n1,...) StorageObject({ EVAL(VECTOR(t1,n1,__VA_ARGS__))})
-
-//this should be used to declare the attributes if the SO_CONSTRUCTOR is used and DEFAULT_CONSTRUCTOR is not used
-#if 0
-#define HECUBA_ATTRS(t1,n1,...) EVAL(DECL(t1,n1,__VA_ARGS__)) 
-#endif
-
-#define HECUBA_ATTRS(t1,n1,...) SO_ClassName myname = {this, typeid(*this).name()}; EVAL(DECL(t1,n1,__VA_ARGS__)) 
-//miso: public StorageObject {
-//      DEFAULT_CONSTRUCTOR(miso,int,a,float,b) {
-//          
-//      }
-//      miso(par1,par2): SO_CONSTRUCTOR(int,a,float,b){
-//      }
-//}
+#define HECUBA_ATTRS(t1,n1,...) SO_ClassName myname = {this, typeid(*this).name()}; EVAL(DECL(t1,n1,__VA_ARGS__))
 //
-//miso: public StorageObject {
-//      DECLARE_ATTRS(int,a,float,b)
-//      miso(par1,par2): SO_CONSTRUCTOR(int,a,float,b) {
+//class miso: public StorageObject {
+//  public:
+//      HECUBA_ATTRS(int,a,float,b)
+//};
 //
-//      }
-//}
+//This expands to:
+//class miso:publicStorageObject
+//class innerSO: public StorageObject {
+//    public:
+//        SO_ClassName myname  ={this, typeid(*this).name()};
+//        SO_Attribute<int>   a={this, "a"};
+//        SO_Attribute<float> b={this, "b"};
+//};
 //
-//Now it works both with constructor or just using DECLARE_ATTRS
 
 //recursive behaviour extracted from http://jhnet.co.uk/articles/cpp_magic
 
