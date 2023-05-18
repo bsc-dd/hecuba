ff697248466c0bafcab576deb1e5e49c46211aba
yolandab <yolanda.becerra@bsc.es>
2023-05-18 12:52:12 +0200

    Add a flag to enable/disable lazy write
        * Lazy writes are enabled for StorageNumpys, but it depend on the
          use case, therefore add a new environment variable
          HECUBA_LAZY_WRITES to enable/disable its use.

--- hecuba_core/src/ArrayDataStore.h
+++ hecuba_core/src/ArrayDataStore.h
@@ -65,6 +65,7 @@ protected:
 
     SpaceFillingCurve partitioner;
 
+    bool has_lazy_writes = false; // flag to enable/disable lazy writes in cassandra (useful if there are multiple writes to the same object)
     bool arrow_enabled = false;
     bool arrow_optane  = false; // Intel OPTANE disk enabled?
     std::string arrow_path  = "";
